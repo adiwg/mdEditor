4482b5d3f70b9f232b6b0207bd481dfe962249df
Josh Bradley <jbradley@arcticlcc.org>
2015-11-01 23:33:08 -0900

    Post init configuration.
    Set pods as default,set prefix.
    Upgrade to ember/ember-data v2.1.0.
    Use latest jquery.
    Set page title to mdEditor.

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'mdeditor',
+    podModulePrefix: 'mdeditor/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
