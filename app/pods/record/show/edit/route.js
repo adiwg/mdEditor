e7e84421653a387cb59ce413e7e081026ad9f579
Josh Bradley <jbradley@arcticlcc.org>
2016-02-02 16:56:31 -0900

    Dynamic configuration of nav-secondary

--- app/pods/record/show/edit/route.js
+++ app/pods/record/show/edit/route.js
@@ -1,16 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  renderTemplate() {
+  navLinks:[{
+      name: 'Keywords',
+      target: 'record.show.edit.keywords'
+
+  },{
+      name: 'Quality',
+      target: 'record.show.edit.quality'
+
+  }],
+  beforeModel (transition) {
+    if (transition.targetName === 'record.show.edit.index') {
+      this.transitionTo('record.show.edit.main');
+    }
+  },
+  renderTemplate (controller, model) {
     this.render('record.show.edit.nav', {
       into: 'record.show.nav'
     });
     this.render('record.show.edit.nav-secondary', {
       into: 'application',
-      outlet: 'nav-secondary'
+      controller: 'application',
+      outlet: 'nav-secondary',
+      model: this.get('navLinks')
     });
     this.render('record.show.edit', {
-      into: 'record'
+      into: 'record',
+      model: model
     });
   }
 });
