72a2abb3d3658124d4fc9f6b467229f91575afda
Josh Bradley <jbradley@arcticlcc.org>
2016-09-07 07:20:05 -0800

    Start export implementation.

--- app/pods/export/route.js
+++ app/pods/export/route.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
+import moment from 'moment';
 
 export default Ember.Route.extend({
+  /*model() {
+    return Ember.Object.create({
+      compress: true
+    });
+  },*/
+compress: true,
+
+  actions: {
+    exportData() {
+      this.get('store').exportData(
+        ['records', 'contacts', 'dictionaries'], {
+          download: true,
+          filename: `my-data-${moment(Date()).format('YYYYMMDD-HHMMSS')}.json`
+        }
+      );
+    },
+    toggleCompress() {
+      this.toggleProperty('compress');
+    }
+  }
 });
