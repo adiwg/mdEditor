d5160559ae5fe709d32585b142f5c117131431b9
Josh Bradley <jbradley@arcticlcc.org>
2016-10-04 18:43:08 -0800

    Add GeoJSON export

--- app/pods/record/show/edit/spatial/route.js
+++ app/pods/record/show/edit/spatial/route.js
@@ -12,7 +12,8 @@ export default Ember.Route.extend({
         "properties": {
           "name": "Dinagat Islands"
         }
-      })]
+      })],
+      featureGroup: null
     });
   },
   actions: {
@@ -23,6 +24,50 @@ export default Ember.Route.extend({
     deleteAllFeatures() {
 
       this.currentModel.set('layers', Ember.A());
+    },
+    setFeatureGroup(obj) {
+      this.currentModel.set('featureGroup', obj);
+    },
+    exportGeoJSON() {
+      let fg = this.currentModel.get('featureGroup');
+
+      let json = {
+        'type': 'FeatureCollection',
+        'features': []
+      };
+
+      if(fg) {
+        let geoGroup = fg.getLayers();
+        geoGroup.forEach((l) => {
+          let layers = l.getLayers();
+
+          layers.forEach((layer) => {
+            let feature = layer.feature;
+
+            json.features.push({
+              'type': 'Feature',
+              'id': feature.id,
+              'geometry': feature.geometry,
+              'properties': feature.properties
+            });
+          });
+        });
+        
+        window.saveAs(
+          new Blob([JSON.stringify(json)], {
+            type: 'application/json;charset=utf-8'
+          }),
+          'export_features.json'
+        );
+
+        // return new Ember.RSVP.Promise((resolve) => {
+        //   Ember.run(null, resolve, json);
+        // }, 'MD: ExportSpatialData');
+
+      } else {
+        Ember.get(this, 'flashMessages')
+          .warning('Found no features to export.');
+      }
     }
   }
 });
