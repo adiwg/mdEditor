1bca7b1b235feb963d7b102b8fd81c3b38a257d8
Josh Bradley <jbradley@arcticlcc.org>
2016-10-11 09:37:06 -0800

    deploy

---
+++ docs/api.js
@@ -0,0 +1,22 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "md-address",
+        "md-boolean",
+        "md-button-modal",
+        "md-codelist",
+        "md-codelist-multi",
+        "md-datetime",
+        "md-input",
+        "md-inputs",
+        "md-object-table",
+        "md-online-resource-array",
+        "md-phone-array",
+        "md-select",
+        "md-textarea"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
