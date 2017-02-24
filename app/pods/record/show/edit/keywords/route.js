b3f41e3b072098343f686ae59cba0ac22545d1a1
Josh Bradley <jbradley@arcticlcc.org>
2017-02-24 10:13:58 -0800

    Add ember-json-tree in-repo addon
    Renamed tree-view addon

--- app/pods/record/show/edit/keywords/route.js
+++ app/pods/record/show/edit/keywords/route.js
@@ -1,4 +1,35 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.A([Ember.Object.create({
+      id: 0,
+      name: 'Root',
+      isExpanded: true,
+      isSelected: false,
+      isVisible: true,
+      children: [{
+        id: 1,
+        name: 'First Child',
+        isExpanded: true,
+        isSelected: false,
+        isVisible: true,
+        children: []
+      }, {
+        id: 2,
+        name: 'Second Child',
+        isExpanded: false,
+        isSelected: false,
+        isVisible: true,
+        children: [{
+          id: 3,
+          name: 'First Grand Child',
+          isExpanded: true,
+          isSelected: true,
+          isVisible: true,
+          children: []
+        }]
+      }]
+    })]);
+  }
 });
