09d7d47821a5dae43548a949bb4d2f5e6a3c2dc5
Josh Bradley <jbradley@arcticlcc.org>
2017-03-27 07:45:56 -0800

    Add md-toggle

---
+++ app/pods/components/input/md-toggle/component.js
@@ -0,0 +1,24 @@
+import Toggle from 'ember-toggle/components/x-toggle/component';
+
+export default Toggle.extend({
+  /**
+  * Custom toggle switch for boolean input
+  *
+  * @class md-toggle
+  * @constructor
+  * @extends ember-toggle/components/x-toggle
+  * @module mdeditor
+  * @submodule components-input
+  */
+
+ /**
+  * Bound classes:
+  *  - value
+  *    - __true__: toggle-on
+  *    - __false__: toggle-off
+  *
+  * @property classNameBindings
+  * @type {Array}
+  */
+  classNameBindings: ['value:toggle-on:toggle-off']
+});
