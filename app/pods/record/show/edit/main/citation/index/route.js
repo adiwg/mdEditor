1ad159dd63fa1d799244e625ce1e4d445d64ac9c
Josh Bradley <jbradley@arcticlcc.org>
2017-06-14 14:17:40 -0800

    Fix cancel action in main.citation

--- app/pods/record/show/edit/main/citation/index/route.js
+++ app/pods/record/show/edit/main/citation/index/route.js
@@ -1,8 +1,44 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+const {
+  Route,
+  get,
+  getWithDefault,
+  set
+} = Ember;
+
+export default Route.extend({
+  afterModel() {
+    this.setupModel();
+  },
+
+  setupController: function () {
+    // Call _super for default behavior
+    this._super(...arguments);
+
+    this.controllerFor('record.show.edit')
+      .setProperties({
+        onCancel: this.setupModel
+      });
+  },
+
+  setupModel() {
+    let model = this.currentRouteModel();
+    let citation = get(model, 'json.metadata.resourceInfo.citation');
+
+    set(citation, 'responsibleParty', getWithDefault(citation,
+      'responsibleParty', []));
+    set(citation, 'presentationForm', getWithDefault(citation,
+      'presentationForm', []));
+    set(citation, 'onlineResource', getWithDefault(citation,
+      'onlineResource', []));
+    set(citation, 'identifier', getWithDefault(citation, 'identifier', []));
+    set(citation, 'graphic', getWithDefault(citation, 'graphic', []));
+    set(citation, 'series', getWithDefault(citation, 'series', {}));
+  },
+
   actions: {
-    editIdentifier(index){
+    editIdentifier(index) {
       this.transitionTo('record.show.edit.main.citation.identifier', index);
 
     }
