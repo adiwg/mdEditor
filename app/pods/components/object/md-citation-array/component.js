232407d7b187807676ead7825bd2f394b3c70d32
Josh Bradley <jbradley@arcticlcc.org>
2017-06-29 20:55:52 -0800

    Add md-citation-array

---
+++ app/pods/components/object/md-citation-array/component.js
@@ -0,0 +1,75 @@
+import Ember from 'ember';
+
+const {
+  A,
+  Component
+} = Ember;
+
+export default Component.extend({
+
+  init() {
+    this._super(...arguments);
+
+    if(!this.get('model')) {
+      this.set('model', A());
+    }
+  },
+
+  /**
+   * mdEditor class for input and edit of mdJSON 'citation' object
+   * arrays.
+   * The class manages the maintenance of an array of citation
+   * objects using the md-object-table class.
+   *
+   * @module mdeditor
+   * @submodule components-object
+   * @class md-citation-array
+   * @constructor
+   */
+
+  attributeBindings: ['data-spy'],
+
+  /**
+   * mdJSON object containing the 'citation' array.
+   *
+   * @property model
+   * @type Object
+   * @required
+   */
+
+  /**
+   * List of mdJSON 'citation' object attributes to display in
+   * md-object-table to aid in choosing the citation to edit or
+   * delete.
+   * The property is passed to md-object-table for configuration.
+   *
+   * @property attributes
+   * @type String
+   * @default 'title'
+   */
+  attributes: 'title',
+
+  /**
+   * Name to place on the mdEditor panel header for entry and edit of
+   * 'citation' objects.
+   * The property is passed to md-object-table for configuration.
+   *
+   * @property label
+   * @type String
+   * @default 'Citation'
+   */
+  label: 'Citation',
+
+  /**
+   * See [md-array-table](md-array-table.html#property_templateClass).
+   *
+   * @property templateClass
+   * @type Ember.Object
+   */
+  templateClass: Ember.Object.extend({
+    init() {
+      this._super(...arguments);
+      //this.set('authority', {});
+    }
+  })
+});
