e7f11bb5a643910dfca8fe2ad0f8a44f0532b62c
Josh Bradley <jbradley@arcticlcc.org>
2017-08-15 20:23:48 -0800

    Update funding

--- app/pods/record/show/edit/funding/index/route.js
+++ app/pods/record/show/edit/funding/index/route.js
@@ -1,4 +1,40 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+const {
+  Route,
+  set,
+  getWithDefault,
+  get,
+  NativeArray
+} = Ember;
+
+export default Route.extend({
+  afterModel(m) {
+    this._super(...arguments);
+
+    let model = get(m, 'json.metadata');
+    set(model, 'funding', NativeArray.apply(getWithDefault(model, 'funding', [])));
+  },
+
+  setupController: function() {
+    // Call _super for default behavior
+    this._super(...arguments);
+
+    this.controller.set('parentModel', this.modelFor(
+      'record.show.edit'));
+  },
+
+  actions: {
+    // editResource(id) {
+    //   this.transitionTo('record.show.edit.associated.resource', id);
+    // } //,
+    // templateClass() {
+    //   return Ember.Object.extend({
+    //     init() {
+    //       this._super(...arguments);
+    //       //this.set('authority', {});
+    //     }
+    //   });
+    // }
+  }
 });
