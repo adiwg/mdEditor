7677f3a9aa444dfc6368e375ff55cfbd13cd6d64
Josh Bradley <jbradley@arcticlcc.org>
2017-10-13 08:12:48 -0800

    Work on publication support

---
+++ tests/unit/utils/config-test.js
@@ -0,0 +1,10 @@
+import config from 'mdeditor/utils/config';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | config');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = config();
+  assert.ok(result);
+});
