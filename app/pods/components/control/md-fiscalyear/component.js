d5a568838d325f9663ffe3f952b2e733ab9e6920
Josh Bradley <jbradley@arcticlcc.org>
2017-10-25 20:08:37 -0700

    Fix md-fiscalyear
    The change event wasn't being triggered in the datatimepicker addon.
    Seeting the value using the datatimepicker fixes the issue.

--- app/pods/components/control/md-fiscalyear/component.js
+++ app/pods/components/control/md-fiscalyear/component.js
@@ -10,12 +10,11 @@ const {
 export default Select.extend({
   layout,
   objectArray: computed(function() {
-    return Array.apply(0, Array(12))
-      .map(function(element, index) {
-        return {
-          year: index + (moment().year() - 10)
-        };
-      });
+    return Array.apply(0, Array(12)).map(function(element, index) {
+      return {
+        year: index + (moment().year() - 10)
+      };
+    });
   }),
   label: 'Fiscal Year',
   valuePath: 'year',
@@ -24,14 +23,24 @@ export default Select.extend({
   searchEnabled: true,
   placeholder: 'Select a Fiscal Year.',
   create: true,
-  change(){
+  change() {
     let val = this.get('value');
-    let start = moment(val, 'YYYY').subtract(1, 'year').month('October').startOf('month');
+    let start = moment(val, 'YYYY').subtract(1, 'year').month('October').startOf(
+      'month');
     let end = moment(val, 'YYYY').month('September').endOf('month');
+    let context = this.get('context');
 
     this.setProperties({
       end: end,
       start: start
     });
+
+    //have to set values using datetimepicker
+    context.$('.start .date')
+      .data("DateTimePicker")
+      .date(start);
+    context.$('.end .date')
+      .data("DateTimePicker")
+      .date(end);
   }
 });
