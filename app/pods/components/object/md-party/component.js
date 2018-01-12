2782fa24e4de26ff3d2eb3cf25a39bff58290e35
Josh Bradley <jbradley@arcticlcc.org>
2018-01-12 07:59:06 -0800

    Add md-party
    Rename md-party to md-party-array
    Add new md-party component

--- app/pods/components/object/md-party/component.js
+++ app/pods/components/object/md-party/component.js
@@ -1,17 +1,22 @@
-import Ember from 'ember';
+import Component from '@ember/component';
+import EmberObject from '@ember/object';
+import {
+  A
+} from '@ember/array';
+import {
+  once
+} from '@ember/runloop';
+import {
+  computed,
+  defineProperty,
+  get,
+  set
+} from '@ember/object';
 import {
   validator,
   buildValidations
 } from 'ember-cp-validations';
-
-const {
-  Component,
-  computed,
-  Object: EmObject,
-  A,
-  get,
-  set
-} = Ember;
+import ObjectTemplate from 'mdeditor/mixins/object-template';
 
 const Validations = buildValidations({
   'role': [
@@ -26,35 +31,68 @@ const Validations = buildValidations({
   })
 });
 
-export default Component.extend({
-  attributeBindings: ['data-spy'],
-
-  /**
-   * See [md-array-table](md-array-table.html#property_templateClass).
-   *
-   * @property templateClass
-   * @type Ember.Object
-   */
-  templateClass: EmObject.extend(Validations, {
-    init() {
-      this._super(...arguments);
-      this.set('party', A());
-      this.set('role', null);
+const Template = EmberObject.extend(Validations, {
+  init() {
+    this._super(...arguments);
+    this.set('party', A());
+    this.set('role', null);
+  },
+  contacts: computed('party', {
+    get() {
+      let party = get(this, 'party');
+      return party.mapBy('contactId');
     },
-    contacts: computed('party', {
-      get() {
-        let party = get(this, 'party');
-        return party.mapBy('contactId');
-      },
-      set(key, value) {
-        let map = value.map((itm) => {
-          return {
-            contactId: itm
-          };
-        });
-        set(this, 'party', map);
-        return value;
-      }
-    })
+    set(key, value) {
+      let map = value.map((itm) => {
+        return {
+          contactId: itm
+        };
+      });
+      set(this, 'party', map);
+      return value;
+    }
   })
 });
+
+const theComp = Component.extend(ObjectTemplate, {
+  init() {
+    this._super(...arguments);
+
+    let path = this.get('path');
+    let model = this.get('model');
+
+    defineProperty(this, 'party', computed(
+      "model."+path,
+      function () {
+        if(model && path) {
+          return get(model, path);
+        }
+        return model;
+      }));
+  },
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+
+    let path = this.get('path');
+    let model = this.get('model');
+
+    if(model && path) {
+      let obj = get(this, 'model.' + path);
+
+      once(this, function () {
+        set(model, path, this.applyTemplate(obj));
+      });
+    }
+  },
+
+  attributeBindings: ['data-spy'],
+  templateClass: Template
+});
+
+export {
+  Validations,
+  Template,
+  theComp as
+  default
+};
--- tests/integration/pods/components/object/md-party-array/component-test.js
+++ tests/integration/pods/components/object/md-party-array/component-test.js
@@ -1,23 +1,24 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('object/md-party', 'Integration | Component | object/md party', {
+moduleForComponent('object/md-party-array', 'Integration | Component | object/md party', {
   integration: true
 });
 
 test('it renders', function(assert) {
+
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{object/md-party}}`);
+  this.render(hbs`{{object/md-party-array}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#object/md-party}}
+    {{#object/md-party-array}}
       template block text
-    {{/object/md-party}}
+    {{/object/md-party-array}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
