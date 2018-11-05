ff13a81c51d50e2aa1c9f0d153670bb8f077cb2a
Josh Bradley <jbradley@arcticlcc.org>
2018-11-05 09:55:16 -0900

    Improve lineage preview

--- app/pods/components/object/md-lineage/preview/component.js
+++ app/pods/components/object/md-lineage/preview/component.js
@@ -1,4 +1,10 @@
-import Ember from 'ember';
+import Component from '@ember/component';
+import { computed } from '@ember/object';
 
-export default Ember.Component.extend({
+export default Component.extend({
+  showMore: false,
+  limit:1,
+  showLimit: computed('limit','showMore', function() {
+    return this.get('showMore') ? 100: this.get('limit');
+  }),
 });
