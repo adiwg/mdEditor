fce3ca8ccb4a2bca8ab68d5eca8deacc46da94a6
Josh Bradley <jbradley@arcticlcc.org>
2018-11-28 12:49:47 -0900

    Persist translate options

--- app/pods/record/show/translate/route.js
+++ app/pods/record/show/translate/route.js
@@ -1,5 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+setupController(controller, model) {
+  this._super(controller, model);
+
+  controller.setProperties({
+    writer: controller.writer || null,
+    forceValid: controller.forceValid || false,
+    showAllTags: controller.showAllTags || false
+  });
+},
 
 });
