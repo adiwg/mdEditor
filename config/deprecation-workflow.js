0a8cdaa647541fb027a12619b98e8eeae4991182
Josh Bradley <jbradley@arcticlcc.org>
2019-03-08 09:49:02 -0800

    Update ember-cli-bootstrap-datetimepicker
    Fixes deprecations.

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,37 +1,15 @@
 /* eslint-env browser */
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
-  workflow: [{
-      handler: "silence",
-      matchId: "ember-routing.route-router"
-    },
-    {
-      handler: "silence",
-      matchId: "ember-component.send-action"
-    },
-    {
-      handler: "silence",
-      matchId: "ember-runtime.deprecate-copy-copyable"
-    },
-    {
-      handler: "silence",
-      matchId: "ember-runtime.using-array-copy"
-    },
-    {
-      handler: "silence",
-      matchId: "ember-metal.getting-each"
-    },
-    {
-      handler: "silence",
-      matchId: "ember-routing.route-router"
-    },
-    {
-      handler: "silence",
-      matchId: "ember-polyfills.deprecate-merge"
-    },
-    {
-      handler: "silence",
-      matchId: "remove-handler-infos"
-    }
+  workflow: [
+    { handler: "log", matchId: "ember-routing.route-router" },
+    { handler: "log", matchId: "ember-data:method-calls-on-destroyed-store" },
+    { handler: "log", matchId: "ember-component.send-action" },
+    { handler: "log", matchId: "ember-runtime.using-array-copy" },
+    { handler: "log", matchId: "ember-runtime.deprecate-copy-copyable" },
+    { handler: "log", matchId: "ember-name-key-usage" },
+    { handler: "log", matchId: "events.remove-all-listeners" },
+    { handler: "log", matchId: "object.new-constructor" },
+    { handler: "log", matchId: "ember-polyfills.deprecate-merge" },
   ]
 };
