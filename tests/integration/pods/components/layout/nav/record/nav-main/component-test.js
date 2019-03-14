18a338878af18809dff6e3e63a711ba9af6416f7
Josh Bradley <jbradley@arcticlcc.org>
2019-03-14 13:35:43 -0700

    Fix record/nav-main test

--- tests/integration/pods/components/layout/nav/record/nav-main/component-test.js
+++ tests/integration/pods/components/layout/nav/record/nav-main/component-test.js
@@ -9,10 +9,13 @@ module('Integration | Component | layout/nav/record/nav-main', function(hooks) {
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
+    this.foo = function() {};
 
-    await render(hbs`{{layout/nav/record/nav-main}}`);
+    await render(hbs`{{layout/nav/record/nav-main}}
+    {{to-elsewhere named="record-nav" send=(component "input/md-select-profile" value="full" updateProfile=this.foo)}}
+    `);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.equal(this.element.textContent.replace(/[\s\n]+/g, '|').trim(), '|Translate|Profile|full|?|');
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +24,6 @@ module('Integration | Component | layout/nav/record/nav-main', function(hooks) {
       {{/layout/nav/record/nav-main}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(this.element.textContent.trim(), 'Translate');
   });
 });
