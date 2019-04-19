e8778492a7ba664d72f529e5de117d472f95c412
Josh Bradley <jbradley@arcticlcc.org>
2019-04-19 10:32:31 -0700

    Add md-button component

---
+++ app/pods/components/control/md-button/component.js
@@ -0,0 +1,78 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  /**
+   * mdEditor Component that renders a `<button>`
+   *
+   * @class md-button
+   * @module mdeditor
+   * @submodule components-control
+   * @constructor
+   */
+
+  tagName: 'button',
+  classNames: ['md-button btn'],
+  classNameBindings: ['responsive:md-btn-responsive'],
+  attributeBindings: ['type', 'disabled'],
+  type: 'button',
+  disabled: null,
+
+  /**
+   * Button text
+   *
+   * @property text
+   * @type {String}
+   * @default ""
+   */
+  text: '',
+
+  /**
+   * Button icon
+   *
+   * @property icon
+   * @type {String}
+   * @default ""
+   */
+  icon: '',
+
+  /**
+   * Tooltip text shown when isShowingConfirm is true
+   *
+   * @property tooltip
+   * @type {String}
+   * @default "undefined"
+   */
+
+  /**
+   * Side to show tooltip
+   *
+   * @property tipSide
+   * @type {String}
+   * @default "left"
+   */
+  tipSide: 'left',
+
+  /**
+   * Class to add to tooltip
+   *
+   * @property tipClass
+   * @type {String}
+   * @default ""
+   */
+  tipClass: '',
+
+  /**
+  * Render with wrapped text. Defaults to true if text.length is > 12 or
+  * contains spaces.
+  *
+  * @property responsive
+  * @type {Boolean}
+  * @default "false"
+  * @category computed
+  * @requires text
+  */
+  responsive: computed('text', function() {
+    return this.text.length > 12 || this.text.indexOf(' ') > 0;
+  }),
+});
