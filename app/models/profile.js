0f9cafd1d080f36210b5ddc052943411d89c0cdf
Josh Bradley <jbradley@arcticlcc.org>
2019-05-20 17:08:04 -0800

    Add profile model

---
+++ app/models/profile.js
@@ -0,0 +1,21 @@
+import DS from 'ember-data';
+
+// [{
+//   "title": "",
+//   "description": "",
+//   "version": "",
+//   "components": {
+//     "record": {},
+//     "contact": {},
+//     "dictionary": {}
+//   },
+//   "nav": [{
+//     "title": "",
+//     "target": "",
+//     "tip": ""
+//   }]
+// }]
+
+export default DS.Model.extend({
+
+});
