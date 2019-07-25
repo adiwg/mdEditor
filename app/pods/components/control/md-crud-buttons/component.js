2e88aea967808bd8910bb2140e90e9fd7c880894
Josh Bradley <jbradley@arcticlcc.org>
2019-07-24 17:34:22 -0800

    Hide Delete and Copy by default in edit mode
    Configurable in Settings.
    
    Closes #280.

--- app/pods/components/control/md-crud-buttons/component.js
+++ app/pods/components/control/md-crud-buttons/component.js
@@ -1,8 +1,38 @@
 /* eslint-disable ember/closure-actions */
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+import { isEmpty } from '@ember/utils';
 
 export default Component.extend({
   classNames: 'md-crud-buttons',
+  settings: service(),
+
+  /**
+   * Indicates whether to display the "Delete" button. If not defined, defaults
+   * to "settings.data.showDelete".
+   *
+   * @property allowDelete
+   * @type {Boolean}
+   * @default "settings.showDelete"
+   */
+
+  /**
+   * Indicates whether to display the "Copy" button. If not defined, defaults to
+   * "settings.data.showDelete".
+   *
+   * @property allowCopy
+   * @type {Boolean}
+   * @default "settings.showDelete"
+   */
+
+  showDelete: computed('settings.showDelete', 'allowDelete', function () {
+    return isEmpty(this.allowDelete) ? this.settings.data.showDelete : this.allowDelete;
+  }),
+
+  showCopy: computed('settings.showDelete', 'allowCopy', function () {
+    return isEmpty(this.allowCopy) ? this.settings.data.showCopy : this.allowCopy;
+  }),
 
   actions: {
     save: function () {
