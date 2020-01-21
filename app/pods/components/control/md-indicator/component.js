565fdaec952a992380d08ded799726bb7a03b2dc
Devon Anderson <dvonanderson@gmail.com>
2020-01-21 11:15:14 -0500

    Validate the user is on specific route
    Resolves #350

--- app/pods/components/control/md-indicator/component.js
+++ app/pods/components/control/md-indicator/component.js
@@ -1,31 +1,26 @@
 import Component from '@ember/component';
+import { computed, get}  from '@ember/object';
 import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
+
 
 export default Component.extend({
   router: service(),
-   /**
-    * Domain indicator's tooltip value for the name of the domain name
-    *
-    * @property domainName
-    * @type {String}
-    */
+  /**
+   *
+   * @property indicatorDomainName
+   * @type {String}
+   * @default null
+   */
+  indicatorDomainName: null,
 
-
-    //!Need to grab model data from dictionary.show route
-   domainName: computed(function () {
-     let model = this.modelFor('dictionary.show')
-     console.log(model);
-   }),
-
-   /**
-    * Render only if route is "dictionary.show.edit.entity"
-    *
-    * @property  entityRoute
-    * @type {Boolean}
-    *
-    */
-   entityRoute: computed('router.currentRouteName', function() {
-     return "dictionary.show.edit.entity.edit.index" === this.router.currentRouteName
-   })
+  /**
+   *
+   * @property userCurrentRoute
+   * @type {String}
+   * @default null
+   */
+  userCurrentRoute: computed('router', function () {
+    let router = get(this, 'router')
+    return router.currentRouteName === "dictionary.show.edit.entity.edit.index"
+  })
 });
