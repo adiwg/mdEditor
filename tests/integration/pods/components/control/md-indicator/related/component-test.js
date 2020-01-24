ac76d9cbf8474ffe391764684e728cf694111d30
Josh Bradley <jbradley@arcticlcc.org>
2020-01-23 19:20:10 -0800

    Refactor md-indicator

---
+++ tests/integration/pods/components/control/md-indicator/related/component-test.js
@@ -0,0 +1,26 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | control/md-indicator/related', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`{{control/md-indicator/related}}`);
+
+    assert.equal(this.element.textContent.trim(), '');
+
+    // Template block usage:
+    await render(hbs`
+      {{#control/md-indicator/related}}
+        template block text
+      {{/control/md-indicator/related}}
+    `);
+
+    assert.equal(this.element.textContent.trim(), 'template block text');
+  });
+});
