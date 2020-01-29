fdc8fa9bbb3ae8ef9113ea18436c2838b4fee820
Devon Anderson <dvonanderson@gmail.com>
2020-01-29 12:09:46 -0500

    Refactored md-interpolate test
    Resolves #350

--- tests/unit/utils/md-interpolate-test.js
+++ tests/unit/utils/md-interpolate-test.js
@@ -1,11 +1,17 @@
-import mdInterpolate from 'mdeditor/utils/md-interpolate';
+import { interpolate, parseArgs} from 'mdeditor/utils/md-interpolate';
 import { module, test } from 'qunit';
 
 module('Unit | Utility | md-interpolate', function() {
 
   // Replace this with your real tests.
   test('it works', function(assert) {
-    let result = mdInterpolate();
+    assert.expect(2)
+    let note = "The attribute <em>this attribute</em> has an associated domain: <strong>this domain</strong>."
+    let result = interpolate(note);
     assert.ok(result);
+
+    let note2 = "The attribute <em>${value1}</em> has an associated domain: <strong>${value2}</strong>."
+    let result2 = parseArgs(note2)
+    assert.ok(result2)
   });
 });
