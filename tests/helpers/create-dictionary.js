05a790473c0d855948807495170740ef7ae19746
Josh Bradley <jbradley@arcticlcc.org>
2020-01-29 16:40:16 -0800

    Refactor tests
    - Improve dictionary helper
     - Update md-indicator, md-interpolate tests

--- tests/helpers/create-dictionary.js
+++ tests/helpers/create-dictionary.js
@@ -1,6 +1,6 @@
 import EmberObject from '@ember/object';
 
-export default function createDictionary(total) {
+let createDictionary = function (total) {
 
   const dictionaries = [];
 
@@ -8,21 +8,21 @@ export default function createDictionary(total) {
 
     const dictionary = EmberObject.create({
 
-      json:  {
+      json: {
         "dataDictionary": {
           "citation": {
-            "title": "My Dictionary",
+            "title": "My Dictionary" + i,
             "date": [{
               "date": new Date()
                 .toISOString(),
               "dateType": "creation"
             }]
           },
-          "description": "Data dictionary.",
+          "description": "Data dictionary." + i,
           subject: [],
           responsibleParty: {},
-          domain: [],
-          entity: []
+          domain: createDomain(2),
+          entity: createEntity(2)
         },
       },
       title: 'My Dictionary' + i,
@@ -35,4 +35,108 @@ export default function createDictionary(total) {
 
   return dictionaries;
 
+};
+
+let createDomain = function (total) {
+
+  const domains = [];
+
+  for(let i = 0; i < total; i++) {
+
+    const domain = EmberObject.create({
+      "domainId": "domainId" + i,
+      "commonName": "commonName" + i,
+      "codeName": "codeName" + i,
+      "description": "description" + i,
+      "domainItem": [{
+        "name": "name" + i,
+        "value": "value" + i,
+        "definition": "definition" + i
+      }]
+    });
+
+    domains.push(domain);
+
+  }
+  return domains;
+
+};
+
+let createAttribute = function (total) {
+
+  const attributes = [];
+
+  for(let i = 0; i < total; i++) {
+
+    const attribute = EmberObject.create({
+      "commonName": "attributeCommonName" + i,
+      "codeName": "attributeCodeName0-" + i,
+      "alias": [
+        "attributeAlias0-" + i
+      ],
+      "definition": "definition" + i,
+      "dataType": "dataType" + i,
+      "allowNull": true,
+      "units": "units" + i,
+      "domainId": "domainId" + i,
+      "minValue": "0" + i,
+      "maxValue": "99"
+    });
+
+    attributes.push(attribute);
+
+  }
+  return attributes;
+
 }
+
+let createEntity = function (total) {
+
+  const entities = [];
+
+  for(let i = 0; i < total; i++) {
+
+    const entity = EmberObject.create({
+      "entityId": "entityId" + i,
+      "commonName": "commonName" + i,
+      "codeName": "codeName" + i,
+      "alias": [
+        "alias0-" + i,
+        "alias1-" + i,
+      ],
+      "definition": "definition" + i,
+      "primaryKeyAttributeCodeName": [
+        "primaryKeyAttributeCodeName0-" + i,
+        "primaryKeyAttributeCodeName1-" + i,
+      ],
+      "index": [{
+        "codeName": "attributeIndex0-" + i,
+        "allowDuplicates": false,
+        "attributeCodeName": [
+          "attributeCodeName0-" + i
+        ]
+      }],
+      "attribute": createAttribute(3),
+      "foreignKey": [{
+        "localAttributeCodeName": [
+          "attributeCommonName0-" + i
+        ],
+        "referencedEntityCodeName": "referencedEntityCodeName0" + i,
+        "referencedAttributeCodeName": [
+          "referencedAttributeCodeName0-" + i
+        ]
+      }],
+      "fieldSeparatorCharacter": ",",
+      "numberOfHeaderLines": 9,
+      "quoteCharacter": "\""
+    });
+
+    entities.push(entity);
+
+  }
+
+  return entities;
+
+};
+
+export { createDomain, createEntity, createAttribute, createDictionary };
--- tests/integration/pods/components/control/md-indicator/component-test.js
+++ tests/integration/pods/components/control/md-indicator/component-test.js
@@ -1,30 +1,35 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, triggerEvent } from '@ember/test-helpers';
+import { assertTooltipContent } from 'ember-tooltips/test-support/dom';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | control/md-indicator', function(hooks) {
+module('Integration | Component | control/md-indicator', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    assert.expect(1)
+  test('it renders', async function (assert) {
+    assert.expect(2)
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
-    // this.set('icon', 'sticky-note')
+    this.set('values', {
+      foo: 'This',
+      bar: 'warning'
+    })
 
-    await render(hbs`{{control/md-indicator
+    await render(hbs `{{control/md-indicator
       icon="sticky-note"
       title="Hello"
-      note="This is a warning"
+      note="\${foo} is a \${bar}"
+      values=values
       type="danger"}}
       `);
 
+    assert.dom('.md-indicator').isVisible({ count: 1 });
 
-    assert.equal(this.element.textContent.trim(), '');
+    await triggerEvent('.md-indicator', 'mouseenter');
 
-    //!Need help with the selector for the mdIndicator to test the triggerEvent
-    //!What assertions need to be represented
-    // await triggerEvent('myIcon', 'mouseEnter')
-
+    assertTooltipContent(assert, {
+      contentString: 'Hello\nThis is a warning',
+    });
   });
 });
