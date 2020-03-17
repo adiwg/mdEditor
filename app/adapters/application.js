d1a7cffa4b8629c761b9ba6f962987a6672956f6
Devon Anderson <dvonanderson@gmail.com>
2020-03-17 11:43:25 -0400

    Added storage monitor service to adapter - this will check local storage size before user updates records to ensure users aren't over limit
    Closes #157

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,12 +1,26 @@
-import Adapter
-from 'ember-local-storage/adapters/adapter';
+import Adapter from 'ember-local-storage/adapters/adapter';
+import { inject as service } from '@ember/service';
+import mdCalcStorage from 'mdeditor/utils/md-calc-storage';
 
 export default Adapter.extend({
-  // //timestamp updates
-  // updateRecord(store, type, snapshot) {
-  //   //let date = new Date();
-  //
-  //   //snapshot.record.set('dateUpdated', date.toISOString());
-  //   return this._super.apply(this,[store, type, snapshot]);
-  // }
+
+  storageMonitor: service(),
+
+  updateRecord(store, type, snapshot) {
+    // calculate current snapshot data and local storage size
+    let snapshotData = mdCalcStorage(this.serialize(snapshot, {includeId: true}))
+    let snapshotType = mdCalcStorage(snapshot.modelName)
+    let localStorageSize = mdCalcStorage(localStorage)
+
+    //get difference between snapshot and local storage
+    let diff = ((snapshotData - snapshotType) + localStorageSize).toFixed(2)
+
+    //only save updates if calculated storage is under 5000 kb
+    if (diff < 5000) {
+      return this._super.apply(this, [store, type, snapshot]);
+      //timestamp updates
+      //let date = new Date();
+      //snapshot.record.set('dateUpdated', date.toISOString());
+    }
+  }
 });
