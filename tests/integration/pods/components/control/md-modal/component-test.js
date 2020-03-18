459d9b10fc0054beccf8111826e476cf5e6a9a72
Josh Bradley <jbradley@arcticlcc.org>
2020-03-18 13:46:42 -0700

    Revert "Re-factor md-modal"
    This reverts commit 258b2c1e0b4f5c760e94bf15777f0849701e6982.

--- tests/integration/pods/components/control/md-modal/component-test.js
+++ tests/integration/pods/components/control/md-modal/component-test.js
@@ -1,89 +1,27 @@
-import {
-  click,
-  render
-} from '@ember/test-helpers';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import registerHelper from '../../../../../helpers/modal-asserts';
 
-registerHelper();
-
-module('Integration | Component | control/md modal', function (hooks) {
+module('Integration | Component | control/md modal', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
+  test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.on('myAction', function(val) { ... });
 
-    await render(
-      hbs `{{control/md-modal isShowing=true message="The message."}}`);
+    await render(hbs`{{control/md-modal isShowing=true}}`);
 
     assert.ok(document.querySelector('.md-modal-container'));
 
-    assert.equal(document.querySelector('.md-modal-container')
-      .textContent.trim(),
-      'The message.');
     // Template block usage:
-    await render(hbs `
+    await render(hbs`
       {{#control/md-modal isShowing=true}}
         template block text
       {{/control/md-modal}}
     `);
 
-    assert.isPresentOnce('.md-modal-overlay');
-
-    assert.equal(document.querySelector('.md-modal-container')
-      .textContent.trim(),
+    assert.equal(document.querySelector('.md-modal-container').textContent.trim(),
       'template block text');
-  });
-
-  test('shows modal and performs actions', async function (assert) {
-
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
-
-    this.showModal=false;
-
-    assert.expect(3);
-
-    this.set('externalAction', (type) => {
-      assert.ok(type, `${type} called`);
-    });
-
-    await render(hbs `
-        {{control/md-button
-          class="btn btn-danger"
-          icon="times"
-          text="Test"
-          click=(action (mut showModal) true)
-        }}
-        {{control/md-modal
-          message="The modal message."
-          confirm=(action externalAction "confirm")
-          showCancel=true
-          cancelType="primary"
-          showConfirm=true
-          confirmLabel="Confirm"
-          confirmType="danger"
-          isShowing=showModal
-          renderInPlace=true
-        }}
-      `);
-
-    await click('.md-button');
-
-    assert.isAbsent('.md-modal-overlay');
-
-    let num = document.querySelectorAll('.md-modal-buttons button').length;
-    let i = 0;
-
-    while(i < num) {
-      let el = document.querySelector('.md-modal-buttons').querySelectorAll('button')[i];
-      await click(el);
-      i++;
-    }
-
-    assert.isAbsent('.ember-modal-dialog');
   });
 });
