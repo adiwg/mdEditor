c5a4b48355b82a5a7aabdd469e35b98c13241715
Devon Anderson <dvonanderson@gmail.com>
2020-03-27 13:47:14 -0400

    Refactored mdObjectSize util
    Closes #157

--- app/utils/md-object-size.js
+++ app/utils/md-object-size.js
@@ -1,6 +1,29 @@
+import { run } from '@ember/runloop';
+
 let mdObjectSize = (obj) => {
   let blob = new Blob([JSON.stringify(obj)]).size / 1000
   return blob;
 }
 
-export default mdObjectSize;
+let storageSize = () => {
+  //value of current storage
+  let currStorageSize = mdObjectSize(window.localStorage)
+  //percent of current local storage
+  let currStoragePercent = ((currStorageSize / 5000) * 100).toFixed(2)
+  return {
+    percent: currStoragePercent,
+    sizeInKB: currStorageSize
+  }
+}
+
+setInterval(() => {
+  run(function () {
+    storageSize()
+  })
+}, 30000);
+
+
+export {
+  mdObjectSize,
+  storageSize
+};
