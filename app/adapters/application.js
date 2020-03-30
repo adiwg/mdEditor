490682e0eb8f12fc0629d34da505c0bedb595b9f
Devon Anderson <dvonanderson@gmail.com>
2020-03-30 12:20:19 -0400

    Removed local storage check from adapter
    Close #157

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,34 +1,12 @@
 import Adapter from 'ember-local-storage/adapters/adapter';
-import { mdObjectSize } from 'mdeditor/utils/md-object-size';
-import { inject as service } from '@ember/service';
 
 export default Adapter.extend({
 
-  flashMessages: service(),
-
-  updateRecord(store, type, snapshot) {
-    let snapshotRecord = mdObjectSize(this.serialize(snapshot, { includeId: true }));
-    let snapshotType = mdObjectSize(snapshot.modelName);
-    let localStorageSize = mdObjectSize(window.localStorage);
-    let diff = ((snapshotRecord - snapshotType) + localStorageSize).toFixed(2);
-
-    if(diff < 5000) {
-      return this._super.apply(this, [store, type, snapshot])
-    } else {
-      let errorMessage = 'Warning! You have exceeded your local storage capacity.  Your recent activity will not be saved.'
-
-      this.flashMessages.danger(`${errorMessage}`, {timeout: 15000, preventDuplicates: true, onDestroy(){} })
-
-      let error = new Error(errorMessage);
-      error.isAdapterError = true;
-      error.code = 'InvalidError';
-      error.errors = [];
-
-      throw error;
-    }
-  }
+  // updateRecord(store, type, snapshot) {
+    //timestamp updates
+    // let date = new Date();
+    //snapshot.record.set('dateUpdated', date.toISOString());
+  //   return this._super.apply([store, snapshot, type]);
+  // }
 });
 
-//timestamp updates
-// let date = new Date();
-//snapshot.record.set('dateUpdated', date.toISOString());
