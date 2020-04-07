1e6c41b3966fbeb860f815d10b747cd7402908b8
Devon Anderson <dvonanderson@gmail.com>
2020-04-07 14:57:46 -0400

    Working on coverage route

---
+++ app/pods/record/show/edit/spatial/coverage/index/route.js
@@ -0,0 +1,6 @@
+import Route from "@ember/routing/route";
+import ScrollTo from 'mdeditor/mixins/scroll-to';
+
+export default Route.extend(ScrollTo, {
+
+});
--- app/pods/record/show/edit/spatial/coverage/route.js
+++ app/pods/record/show/edit/spatial/coverage/route.js
@@ -1,9 +1,57 @@
 import Route from '@ember/routing/route';
+import { computed } from "@ember/object";
+import { isArray } from '@ember/array';
+import { isEmpty } from '@ember/utils';
 
 export default Route.extend({
   model(params) {
     this.set('coverageId', params.coverage_id)
 
     return this.setupModel();
+  },
+
+  breadCrumb: computed('coverage_id', function () {
+    return{
+      title: 'COVERAGE ' + this.coverage_id,
+      linkable: true
+    };
+  }),
+
+  setupController: function () {
+    this._super(...arguments);
+
+    this.controller.set('parentModel', this.modelFor('record.show.edit'));
+    this.controller.set('coverageId', this.coverageId);
+    this.controllerFor('record.show.edit')
+      .setProperties({
+        onCancel: this.setupModel,
+        cancelScope: this
+      });
+  },
+
+  setupModel() {
+    let coverageId = this.coverageId
+    let model = this.modelFor('record.show.edit');
+    let coverageArray = model.get(
+      'json.metadata.resourseInfo.coverageDescription');
+    let coverage = coverageId && isArray(coverageArray)
+      ? coverageArray.get(coverageId) : undefined;
+
+    // making sure the coverage exists
+    if(isEmpty(coverage)) {
+      this.flashMessages
+        .warning('No Coverage Description Found! Re-directing...');
+      this.replaceWith('record.show.edit.spatial');
+
+      return;
+    }
+
+    return coverage;
+  },
+
+  actions: {
+    parentModel() {
+      return this.modelFor('record.show.edit');
+    }
   }
 });
