2073d0547f1b6791e1b05b889bad6fdb648634a7
Devon Anderson <dvonanderson@gmail.com>
2020-04-08 14:11:35 -0400

    Working through the view of md-coverage object

--- app/pods/components/object/md-coverage/component.js
+++ app/pods/components/object/md-coverage/component.js
@@ -1,5 +1,38 @@
 import Component from '@ember/component';
+import { once } from '@ember/runloop';
+import { alias } from '@ember/object/computed';
+import { get, set, getWithDefault } from '@ember/object';
 import { validator, buildValidations } from "ember-cp-validations";
 
-export default Component.extend({
+const MESSAGE = 'Coverage name and coverage description are required';
+const OPTS = {
+  validate(value, options, model) {
+    return model.notEmpty || MESSAGE
+  }
+}
+const Validations = buildValidations({
+  'covgeName': validator('inline', OPTS),
+  'covgeDesc': validator('inline', OPTS)
+})
+
+export default Component.extend(Validations, {
+  tagName: 'form',
+  didReceiveAttrs() {
+    this._super(...arguments);
+
+    let model = get(this, 'model');
+
+    if(model) {
+      once(this, function () {
+        set(model, 'processingLevelCode', getWithDefault(model, 'processingLevelCode', {}));
+      })
+    }
+  },
+
+  classNames: ['form'],
+  covgeName: alias('model.coverageName'),
+  covgeDesc: alias('model.coverageDescription'),
+  covgeDescId: alias('model.processingLevelCode.identifier'),
+  atrrGroup: alias('model.attributeGroup'),
+  imgDesc: alias('model.imageDescription')
 });
