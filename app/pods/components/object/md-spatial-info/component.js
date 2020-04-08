b17f92f04689fcd3990bbb4923ae271caf6f8793
Devon Anderson <dvonanderson@gmail.com>
2020-04-08 10:54:58 -0400

    Added basic logic and model data for coverage description

--- app/pods/components/object/md-spatial-info/component.js
+++ app/pods/components/object/md-spatial-info/component.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 import { once } from '@ember/runloop';
 import { set, getWithDefault, get } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
   didReceiveAttrs() {
@@ -12,8 +13,13 @@ export default Component.extend({
       set(model, 'spatialReferenceSystem', getWithDefault(model, 'spatialReferenceSystem', []));
       set(model, 'spatialRepresentationType', getWithDefault(model, 'spatialRepresentationType', []));
       set(model, 'spatialResolution', getWithDefault(model, 'spatialResolution', []));
+      set(model, 'coverageDescription', getWithDefault(model,
+        'coverageDescription', []));
     });
   },
+
+  router: service(),
+
   /**
    * The string representing the path in the profile object for the resource.
    *
@@ -31,5 +37,11 @@ export default Component.extend({
    * @required
    */
 
-  tagName: 'form'
+  tagName: 'form',
+
+  actions: {
+    editCoverage(id) {
+      this.router.transitionTo('record.show.edit.spatial.coverage', this.parentModel.id, id)
+    }
+  }
 });
