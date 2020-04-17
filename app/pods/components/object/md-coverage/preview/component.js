0db7fba0dd9737e1dac858bf90e0e38c74ca7921
Devon Anderson <dvonanderson@gmail.com>
2020-04-17 08:53:42 -0400

    Setup md-coverage/preview component

---
+++ app/pods/components/object/md-coverage/preview/component.js
@@ -0,0 +1,10 @@
+import Component from '@ember/component';
+import { alias } from '@ember/object/computed';
+import { Validations } from '../component';
+
+export default Component.extend(Validations, {
+  tagName: '',
+  model: alias('item'),
+  name: alias('model.coverageName'),
+  description: alias('model.coverageDescription')
+});
