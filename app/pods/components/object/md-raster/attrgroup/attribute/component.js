c038f8c506e6b7731599271253411e54441ed931
Devon Anderson <dvonanderson@gmail.com>
2020-06-22 11:03:20 -0400

    Added attribute route, and component

---
+++ app/pods/components/object/md-raster/attrgroup/attribute/component.js
@@ -0,0 +1,77 @@
+import Component from '@ember/component';
+import { once } from '@ember/runloop';
+import { alias } from '@ember/object/computed';
+import { get, set, getWithDefault } from '@ember/object';
+import { ucWords } from 'mdeditor/helpers/uc-words';
+import { decamelize } from '@ember/string';
+import Attribute from 'mdjson-schemas/resources/js/attribute';
+import { validator, buildValidations } from "ember-cp-validations";
+
+const Validations = buildValidations({
+  'attrIdentifier': [
+    validator('presence', {
+      presence: true,
+      ignoreBlank: true
+    })
+  ]
+});
+
+const params = {
+  sequenceIdentifier: 'string',
+  sequenceIdentifierType: 'string',
+  minValue: 'number',
+  maxValue: 'number',
+  units: 'string',
+  scaleFactor: 'number',
+  offset: 'number',
+  meanValue: 'number',
+  numberOfValues: 'number',
+  standardDeviation: 'number',
+  bitsPerValue: 'number',
+  boundMin: 'number',
+  boundMax: 'number',
+  boundUnits: 'number',
+  peakResponse: 'number',
+  toneGradations: 'number',
+  nominalSpatialResolution: 'number',
+}
+
+export default Component.extend(Validations, {
+  init() {
+    this.params = Object.keys(params).map(p => {
+      return {
+        property: p,
+        label: ucWords([decamelize(p).replace(/_/g,
+          ' ')], {
+          force: false
+        }),
+        type: params[p],
+        description: get(Attribute, `properties.${p}.description`)
+      }
+    });
+
+    this._super(...arguments);
+  },
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+
+    let model = getWithDefault(this, 'model', []) || [];
+
+    once(this, function () {
+      set(model, 'attributeIdentifier', getWithDefault(model,
+        'attributeIdentifier', []));
+      set(model, 'bandBoundaryDefinition', getWithDefault(model,
+      'bandBoundaryDefinition', []));
+      set(model, 'transferFunctionType', getWithDefault(model,
+        'transferFunctionType', []));
+      set(model, 'transmittedPolarization', getWithDefault(model,
+        'transmittedPolarization', []));
+      set(model, 'detectedPolarization', getWithDefault(model,
+        'detectedPolarization', []));
+    });
+  },
+  tagName: 'form',
+  attrIdentifier: alias('model.attributeIdentifier.identifier'),
+  attrDesc: alias('model.attriuteDescription'),
+});
---
+++ tests/integration/pods/components/object/md-raster/attrgroup/component-test.js
@@ -0,0 +1,26 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | object/md-raster/attrgroup', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`{{object/md-raster/attrgroup}}`);
+
+    assert.equal(this.element.textContent.trim(), '');
+
+    // Template block usage:
+    await render(hbs`
+      {{#object/md-raster/attrgroup}}
+        template block text
+      {{/object/md-raster/attrgroup}}
+    `);
+
+    assert.equal(this.element.textContent.trim(), 'template block text');
+  });
+});
