bbb466d32fc52d7aef4d69b665936bfa6b9714ed
Devon Anderson <dvonanderson@gmail.com>
2020-07-13 15:43:02 -0400

    Testing TravisCI

--- tests/integration/pods/components/object/md-raster/component-test.js
+++ tests/integration/pods/components/object/md-raster/component-test.js
@@ -11,7 +11,7 @@ module('Integration | Component | object/md-raster', function(hooks) {
     await render(hbs`{{object/md-raster profilePath="foobar" model=raster}}`);
 
     assert.equal(this.element.textContent.replace(/[\s\n]+/g, '|').trim(),
-      '|Name|Description|Attribute|Groups|Add|Add|Attribute|Groups|Processing|Level|Code|Identifier|Namespace|Select|or|type|a|namespace|for|the|identifier.|More|Image|Description|Image|Quality|Code|Identifier|Namespace|Select|or|type|a|namespace|for|the|identifier.|More|Illumination|Elevation|Angle|Illumination|Azimuth|Angle|Imaging|Condition|Cloud|Cover|Percent|Compression|Quantity|Triangulation|Indicator|Radiometric|Calibration|Available|Camera|Calibration|Available|Film|Distortion|Available|Lens|Distortion|Available|', 'md-raster component render'
+      '|Name|Description|Attribute|Groups|Add|Add|Attribute|Groups|Processing|Level|Code|Identifier|Namespace|Select|or|type|a|namespace|for|the|identifier.|More|Image|Description|Image|Quality|Code|Identifier|Namespace|Select|or|type|a|namespace|for|the|identifier.|More|Illumination|Elevation|Angle|Illumination|Azimuth|Angle|Imaging|Condition|Cloud|Cover|Percent|Compression|Quantity|Triangulation|Indicator|Radiometric|Calibration|Available|Camera|Calibration|Available|Film|Distortion|Available|Lens|Distortion|Available|', 'md-raster component renders'
     );
   });
 });
