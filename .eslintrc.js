36800cfa750a8464322b9898cfda5313ce4a24e4
Devon Anderson <dvonanderson@gmail.com>
2021-02-05 09:29:42 -0500

    fixed eslint for debounce errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,62 +1,75 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-"use strict";
+'use strict';
 
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module',
     ecmaFeatures: {
-      legacyDecorators: true
-    }
+      legacyDecorators: true,
+    },
   },
-  plugins: ["ember", "prettier"],
+  plugins: ['ember', 'prettier'],
   extends: [
-    "eslint:recommended",
-    "plugin:ember/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended"
+    'eslint:recommended',
+    'plugin:ember/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   env: {
-    browser: true
+    browser: true,
   },
   rules: {
-    "no-console": 1,
-    "ember/classic-decorator-hooks": "error",
-    "ember/classic-decorator-no-classic-methods": "error"
+    'no-console': 1,
+    'ember/classic-decorator-hooks': 'error',
+    'ember/classic-decorator-no-classic-methods': 'error',
+    'no-incorrect-calls-with-inline-anonymous-functions': 'off',
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
   overrides: [
     // node files
     {
       files: [
-        ".eslintrc.js",
-        ".template-lintrc.js",
-        "ember-cli-build.js",
-        "testem.js",
-        "blueprints/*/index.js",
-        "config/**/*.js",
-        "lib/*/index.js",
-        "server/**/*.js"
+        '.eslintrc.js',
+        '.template-lintrc.js',
+        'ember-cli-build.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'lib/*/index.js',
+        'server/**/*.js',
+        '*.ts',
+        '*.tsx',
       ],
+      excludedFiles: ['app/**'],
       parserOptions: {
-        sourceType: "script",
-        ecmaVersion: 2015
+        sourceType: 'script',
+        ecmaVersion: 2015,
       },
       env: {
         browser: false,
         node: true,
-        es6: true
+        es6: true,
       },
-      plugins: ["node"],
-      extends: ["plugin:node/recommended"],
-      rules: Object.assign({}, require("eslint-plugin-node").configs.recommended.rules, {
-        // add your custom rules and overrides for node files here
+      plugins: ['node'],
+      extends: ['plugin:node/recommended'],
+      rules: Object.assign(
+        {},
+        require('eslint-plugin-node').configs.recommended.rules,
+        {
+          // add your custom rules and overrides for node files here
 
-        // this can be removed once the following is fixed
-        // https://github.com/mysticatea/eslint-plugin-node/issues/77
-        "node/no-unpublished-require": "off"
-      })
-    }
-  ]
+          // this can be removed once the following is fixed
+          // https://github.com/mysticatea/eslint-plugin-node/issues/77
+          'node/no-unpublished-require': 'off',
+          '@typescript-eslint/explicit-module-boundary-types': ['error'],
+          '@typescript-eslint/no-empty-function': ['error'],
+        }
+      ),
+    },
+  ],
 };
