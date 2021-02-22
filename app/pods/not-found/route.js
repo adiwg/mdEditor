5ef44a0ec5cca83cd184311c53bf314edcd669d6
Devon Anderson <dvonanderson@gmail.com>
2021-02-22 18:51:55 -0500

    stage 1 codemods not-found route converted - native class with @classic decorator - angle bracket notation - linting

--- app/pods/not-found/route.js
+++ app/pods/not-found/route.js
@@ -1,27 +1,29 @@
+import classic from 'ember-classic-decorator';
 import EmberObject from '@ember/object';
 import Route from '@ember/routing/route';
 
-export default Route.extend({
+@classic
+export default class NotFoundRoute extends Route {
   model(params) {
     return EmberObject.create({
-      path: params.path
+      path: params.path,
     });
-  },
+  }
 
   init() {
-    this._super(...arguments);
+    super.init(...arguments);
 
     this.breadCrumb = {
       title: 'Page Not Found',
-      linkable: false
+      linkable: false,
     };
-  },
+  }
 
   redirect() {
     var url = this.router.location.formatURL('/not-found');
 
-    if(window.location.pathname !== url) {
+    if (window.location.pathname !== url) {
       this.transitionTo('/not-found');
     }
   }
-});
+}
