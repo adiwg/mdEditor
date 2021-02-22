b0050febd589dd103fedbc5a372bc0fcddf814aa
Devon Anderson <dvonanderson@gmail.com>
2021-02-22 12:26:56 -0500

    stage 1 codemods dictionaries route converted - native class with @classic decorator - angle bracket notation - linting

--- app/pods/dictionaries/route.js
+++ app/pods/dictionaries/route.js
@@ -1,41 +1,50 @@
-import Route from '@ember/routing/route';
+import classic from 'ember-classic-decorator';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import Route from '@ember/routing/route';
 
-const columns = [{
-  propertyName: 'title',
-  title: 'Title'
-},{
-  propertyName: 'dictionaryId',
-  title: 'ID',
-  isHidden: true
-}, {
-  propertyName: 'json.dataDictionary.subject',
-  title: 'Subject'
-}];
+const columns = [
+  {
+    propertyName: 'title',
+    title: 'Title',
+  },
+  {
+    propertyName: 'dictionaryId',
+    title: 'ID',
+    isHidden: true,
+  },
+  {
+    propertyName: 'json.dataDictionary.subject',
+    title: 'Subject',
+  },
+];
 
-export default Route.extend({
-  slider: service(),
+@classic
+export default class DictionariesRoute extends Route {
+  @service
+  slider;
+
   model() {
     //return this.store.peekAll('contact');
-    return this.modelFor('application').findBy('modelName','dictionary');
-  },
-
-  columns: columns,
-
-  actions: {
-    getColumns(){
-      return this.columns;
-    },
-
-    showSlider(rec, evt) {
-      let slider = this.slider;
-
-      evt.stopPropagation();
-      this.controller.set('errorTarget', rec);
-      slider.set('fromName', 'md-slider-error');
-      slider.toggleSlider(true);
-
-      return false;
-    }
+    return this.modelFor('application').findBy('modelName', 'dictionary');
   }
-});
+
+  columns = columns;
+
+  @action
+  getColumns() {
+    return this.columns;
+  }
+
+  @action
+  showSlider(rec, evt) {
+    let slider = this.slider;
+
+    evt.stopPropagation();
+    this.controller.set('errorTarget', rec);
+    slider.set('fromName', 'md-slider-error');
+    slider.toggleSlider(true);
+
+    return false;
+  }
+}
