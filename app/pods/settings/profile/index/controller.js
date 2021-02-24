5210e3ebe6caabcf4c4bb2b16a18c604affa7ea5
Devon Anderson <dvonanderson@gmail.com>
2021-02-24 16:17:15 -0500

    stage 1 codemods settings route converted - native class with @classic decorator - angle bracket notation - linting

--- app/pods/settings/profile/index/controller.js
+++ app/pods/settings/profile/index/controller.js
@@ -1,52 +1,58 @@
-import Controller from '@ember/controller';
+import classic from 'ember-classic-decorator';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import Controller from '@ember/controller';
 
-export default Controller.extend({
-  customProfiles: service('custom-profile'),
+@classic
+export default class IndexController extends Controller {
+  @service('custom-profile')
+  customProfiles;
+
   /* eslint-disable ember/avoid-leaking-state-in-ember-objects */
-  columns: [{
-    propertyName: 'title',
-    title: 'Title'
-  }, {
-    propertyName: 'definition.title',
-    title: 'Definition'
-  }, {
-    propertyName: 'description',
-    title: 'Description',
-    truncate: true,
-    isHidden: false
-  }],
-
-  // columnSets: [],
-  //
-  // badges: [{
-  //   type: 'info',
-  //   icon: 'info-circle',
-  //   tip: 'Update available.',
-  //   isVisible: 'hasUpdate'
-  // }],
-  actions: {
-    addProfile() {
-      this.set('profile', this.store.createRecord('custom-profile'));
+  columns = [
+    {
+      propertyName: 'title',
+      title: 'Title',
     },
-    editProfile(index, record) {
-      this.set('profile', record);
+    {
+      propertyName: 'definition.title',
+      title: 'Definition',
     },
-    saveProfile() {
-      let profile = this.profile;
-
-      return profile.save();
-
+    {
+      propertyName: 'description',
+      title: 'Description',
+      truncate: true,
+      isHidden: false,
     },
+  ];
 
-    cancelEdit() {
-      let record = this.profile;
-
-      this.set('profile', null);
-      record.rollbackAttributes();
-    },
-    manageDefinitions() {
-      this.transitionToRoute('settings.profile.manage');
-    }
+  @action
+  addProfile() {
+    this.set('profile', this.store.createRecord('custom-profile'));
   }
-});
+
+  @action
+  editProfile(index, record) {
+    this.set('profile', record);
+  }
+
+  @action
+  saveProfile() {
+    let profile = this.profile;
+
+    return profile.save();
+  }
+
+  @action
+  cancelEdit() {
+    let record = this.profile;
+
+    this.set('profile', null);
+    record.rollbackAttributes();
+  }
+
+  @action
+  manageDefinitions() {
+    this.transitionToRoute('settings.profile.manage');
+  }
+}
--- app/pods/settings/route.js
+++ app/pods/settings/route.js
@@ -1,71 +1,85 @@
+import classic from 'ember-classic-decorator';
+import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 import Setting from 'mdeditor/models/setting';
-import { get } from '@ember/object';
-import { inject as service } from '@ember/service';
+import { get, action } from '@ember/object';
 
-export default Route.extend({
+@classic
+export default class SettingsRoute extends Route {
+  @service
+  settings;
 
-  settings: service(),
-  publish: service(),
+  @service
+  publish;
+
   /**
    * The profile service
    *
    * @return {Ember.Service} profile
    */
-  profile: service(),
+  @service
+  profile;
 
   setupController(controller, model) {
-    this._super(controller, model);
+    super.setupController(controller, model);
 
-    const links = [{
-      title: 'Main',
-      target: 'settings.main',
-      tip: 'Main application settings'
-    }, {
-      title: 'Profiles',
-      target: 'settings.profile',
-      tip: 'Custom profile settings'
-    }, {
-      title: 'Validation',
-      target: 'settings.validation',
-      tip: 'Custom validation settings'
-    }]
+    const links = [
+      {
+        title: 'Main',
+        target: 'settings.main',
+        tip: 'Main application settings',
+      },
+      {
+        title: 'Profiles',
+        target: 'settings.profile',
+        tip: 'Custom profile settings',
+      },
+      {
+        title: 'Validation',
+        target: 'settings.validation',
+        tip: 'Custom validation settings',
+      },
+    ];
     controller.set('links', links);
-  },
-
-  actions: {
-    clearLocalStorage() {
-      let data = this.settings.data.serialize({ includeId: true });
-
-      window.localStorage.clear();
-
-      if(this.settings.data.keepSettings) {
-
-        window.localStorage.setItem('index-settings',
-          `["settings-${data.data.id}"]`);
-        this.store.pushPayload('setting', data);
-
-        let rec = this.store.peekRecord('setting', data.data.id);
-        rec.save().then(() => window.location.reload());
-      }
-
-      window.location.reload();
-      //this.transitionTo('application');
-    },
-    save() {
-      this.settings.data.save();
-    },
-
-    catalogs() {
-      return this.get('publish.catalogs');
-    },
-
-    resetMdTranslatorAPI() {
-      let url = get(Setting, 'attributes').get('mdTranslatorAPI').options
-        .defaultValue;
-      let model = get(this.controller, 'model');
-
-      model.set('mdTranslatorAPI', url);
-    }
   }
-});
+
+  @action
+  clearLocalStorage() {
+    let data = this.settings.data.serialize({ includeId: true });
+
+    window.localStorage.clear();
+
+    if (this.settings.data.keepSettings) {
+      window.localStorage.setItem(
+        'index-settings',
+        `["settings-${data.data.id}"]`
+      );
+      this.store.pushPayload('setting', data);
+
+      let rec = this.store.peekRecord('setting', data.data.id);
+      rec.save().then(() => window.location.reload());
+    }
+
+    window.location.reload();
+    //this.transitionTo('application');
+  }
+
+  @action
+  save() {
+    this.settings.data.save();
+  }
+
+  @action
+  catalogs() {
+    return this.publish.catalogs;
+  }
+
+  @action
+  resetMdTranslatorAPI() {
+    let url = get(Setting, 'attributes').get('mdTranslatorAPI').options
+      .defaultValue;
+    let model = get(this.controller, 'model');
+
+    model.set('mdTranslatorAPI', url);
+  }
+}
