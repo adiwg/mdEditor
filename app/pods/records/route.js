d47a051b1f5c8e8dea14319422216b9a53e3c1c0
Devon Anderson <dvonanderson@gmail.com>
2021-02-24 14:48:15 -0500

    stage 1 codemods records route converted - native class with @classic decorator - angle bracket notation - linting

--- app/pods/records/route.js
+++ app/pods/records/route.js
@@ -1,43 +1,50 @@
+import classic from 'ember-classic-decorator';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
-import {
-  inject as service
-} from '@ember/service';
 
-const columns = [{
-  propertyName: 'title',
-  title: 'Title'
-}, {
-  propertyName: 'defaultType',
-  title: 'Resource Type',
-  filterWithSelect: true
-}, {
-  propertyName: 'recordId',
-  title: 'ID'
-}]
+const columns = [
+  {
+    propertyName: 'title',
+    title: 'Title',
+  },
+  {
+    propertyName: 'defaultType',
+    title: 'Resource Type',
+    filterWithSelect: true,
+  },
+  {
+    propertyName: 'recordId',
+    title: 'ID',
+  },
+];
 
-export default Route.extend({
-  slider: service(),
+@classic
+export default class RecordsRoute extends Route {
+  @service
+  slider;
+
   model() {
     //return this.store.peekAll('contact');
     return this.modelFor('application').findBy('modelName', 'record');
-  },
-
-  columns: columns,
-
-  actions: {
-    getColumns() {
-      return this.columns;
-    },
-
-    showSlider(rec, evt) {
-      let slider = this.slider;
-
-      evt.stopPropagation();
-      this.controller.set('errorTarget', rec);
-      slider.set('fromName', 'md-slider-error');
-      slider.toggleSlider(true);
-
-      return false;
-    }
   }
-});
+
+  columns = columns;
+
+  @action
+  getColumns() {
+    return this.columns;
+  }
+
+  @action
+  showSlider(rec, evt) {
+    let slider = this.slider;
+
+    evt.stopPropagation();
+    this.controller.set('errorTarget', rec);
+    slider.set('fromName', 'md-slider-error');
+    slider.toggleSlider(true);
+
+    return false;
+  }
+}
