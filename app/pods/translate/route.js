260d4cf9d5ce04ff0b17cd7b98365df9232f2005
Devon Anderson <dvonanderson@gmail.com>
2021-02-24 16:18:51 -0500

    stage 1 codemods translate route converted - native class with @classic decorator - angle bracket notation - linting

--- app/pods/translate/route.js
+++ app/pods/translate/route.js
@@ -1,4 +1,5 @@
 import Route from '@ember/routing/route';
+import classic from 'ember-classic-decorator';
 
-export default Route.extend({
-});
+@classic
+export default class TranslateRoute extends Route {}
