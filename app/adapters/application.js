85fe3209c95e9313d65766c938e294ac213038c3
Devon Anderson <devon_anderson@fws.gov>
2022-08-15 09:36:16 -0400

    fixed folder structure and upgrade packages

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,12 +1,4 @@
 import Adapter
 from 'ember-local-storage/adapters/adapter';
 
-export default Adapter.extend({
-  // //timestamp updates
-  // updateRecord(store, type, snapshot) {
-  //   //let date = new Date();
-  //
-  //   //snapshot.record.set('dateUpdated', date.toISOString());
-  //   return this._super.apply(this,[store, type, snapshot]);
-  // }
-});
+export default Adapter.extend({});
