7a4a10554cfa3477e816542ee567d4a94abae254
dvonanderson <devon.anderson@hashicorp.com>
2023-01-23 16:11:32 -0500

    step before upgrading to glimmer components

--- app/pods/settings/route.js
+++ app/pods/settings/route.js
@@ -1,21 +1,27 @@
+import classic from 'ember-classic-decorator';
+import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 import Setting from 'mdeditor/models/setting';
-import { get } from '@ember/object';
-import { inject as service } from '@ember/service';
+import { get, action } from '@ember/object';
 
-export default Route.extend({
+@classic
+export default class SettingsRoute extends Route {
+  @service
+  settings;
 
-  settings: service(),
-  publish: service(),
+  @service
+  publish;
+
   /**
    * The profile service
    *
    * @return {Ember.Service} profile
    */
-  profile: service(),
+  @service
+  profile;
 
   setupController(controller, model) {
-    this._super(controller, model);
+    super.setupController(controller, model);
 
     const links = [{
       title: 'Main',
@@ -31,27 +37,30 @@ export default Route.extend({
       tip: 'Custom validation settings'
     }]
     controller.set('links', links);
-  },
-
-  actions: {
-    clearLocalStorage() {
-      window.localStorage.clear();
-      this.transitionTo('application');
-      window.location.reload();
-    },
-    save() {
-      this.settings.data.save();
-    },
-
-    catalogs() {
-      return this.get('publish.catalogs');
-    },
-
-    resetMdTranslatorAPI() {
-      let url = get(Setting, 'attributes').get('mdTranslatorAPI').options.defaultValue;
-      let model = get(this.controller, 'model');
-
-      model.set('mdTranslatorAPI', url);
-    }
   }
-});
+
+  @action
+  clearLocalStorage() {
+    window.localStorage.clear();
+    this.transitionTo('application');
+    window.location.reload();
+  }
+
+  @action
+  save() {
+    this.settings.data.save();
+  }
+
+  @action
+  catalogs() {
+    return this.get('publish.catalogs');
+  }
+
+  @action
+  resetMdTranslatorAPI() {
+    let url = get(Setting, 'attributes').get('mdTranslatorAPI').options.defaultValue;
+    let model = get(this.controller, 'model');
+
+    model.set('mdTranslatorAPI', url);
+  }
+}
