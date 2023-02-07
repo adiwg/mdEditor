ee90fd165a7356e582c06fb2f5b5b1790727e879
Tim Zwolak <timothypage@gmail.com>
2023-02-07 11:19:38 -0700

    wip commit

--- app/pods/components/object/md-quality-report/component.js
+++ app/pods/components/object/md-quality-report/component.js
@@ -6,6 +6,52 @@ export default Component.extend({
   actions: {
     deleteReport(index) {
       set(this, 'model', this.model.slice(index));
+    },
+    addReport() {
+      if (this.model == null) {
+        set(this, 'model', [{}]);
+      } else {
+        set(this, 'model', [...this.model, {}])
+      }
+    },
+
+    addToReport(objectKey, index) {
+      set(this, 'model', this.model.map((item, i) =>
+        i === index ? {
+          ...item,
+          [objectKey]: [
+            ...(item[objectKey] || []),
+            {}
+          ]
+        } : item
+      ));
+    },
+
+    removeFromReport(objectKey, reportIndex, objectIndex) {
+      set(this, 'model', this.model.map((item, i) =>
+        i === reportIndex ? {
+          ...item,
+          [objectKey]: item[objectKey].filter((item, i) => i !== objectIndex)
+        } : item
+      ));
+    },
+
+    addQualityMeasureToReport(index) {
+      set(this, 'model', this.model.map((item, i) =>
+        i === index ? {
+          ...item,
+          qualityMeasure: {}
+        } : item
+      ));
+    },
+
+    removeQualityMeasureFromReport(index) {
+      set(this, 'model', this.model.map((item, i) =>
+        i === index ? {
+          ...item,
+          qualityMeasure: undefined
+        } : item
+      ));
     }
   }
 })
