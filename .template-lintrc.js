ec466ed30aa5f1726a462dfe920faa48dbed5d28
dvonanderson <devon.anderson@hashicorp.com>
2023-06-14 15:30:01 -0400

    lint fixes for hbs

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,23 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended',
+  plugins: ['ember-template-lint-plugin-prettier'],
+  extends: ['recommended', 'ember-template-lint-plugin-prettier:recommended'],
+  rules: {
+    'no-passed-in-event-handlers': false,
+    'no-bare-strings': true,
+    'no-curly-component-invocation': {
+      allow: ['app-name', 'company-name', 'company-copyright'],
+    },
+    'no-implicit-this': {
+      allow: [
+        'app-name',
+        'company-name',
+        'company-copyright',
+        'doc-url',
+        'is-loading',
+      ],
+    },
+    'no-route-action': false,
+  },
 };
