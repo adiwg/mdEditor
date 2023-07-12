ac71075a460d5833e8a9c563a196e542248d543b
Johnathan Aspinwall <jwaspin@gmail.com>
2023-07-12 15:20:55 -0600

    Reverting Changes

--- app/pods/record/show/edit/keywords/index/route.js
+++ app/pods/record/show/edit/keywords/index/route.js
@@ -1,54 +1,36 @@
+import { inject as service } from '@ember/service';
+import Route from '@ember/routing/route';
 import { A } from '@ember/array';
 import { getWithDefault, set } from '@ember/object';
-import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 import { copy } from 'ember-copy';
 import $ from 'jquery';
 import ScrollTo from 'mdeditor/mixins/scroll-to';
+// import { on } from '@ember/object/evented';
 
 export default Route.extend(ScrollTo, {
   keyword: service(),
-  profileService: service('profile'),
   model() {
-    let allVocabularies = this.keyword.thesaurus;
-
-    let editModel = this.modelFor('record.show.edit');
-    let json = editModel.get('json');
+    let model = this.modelFor('record.show.edit');
+    let json = model.get('json');
     let info = json.metadata.resourceInfo;
-    let currentVocabularies = info.keyword || [];
 
-    let showModel = this.modelFor('record.show');
-    let profileId = showModel.profile;
-    let profiles = this.profileService.profiles;
-    let selectedProfile = profiles.find((profile) => `${profile.namespace}.${profile.identifier}` === profileId);
+    set(info, 'keyword', !info.hasOwnProperty('keyword') ? A() :
+      A(
+        info.keyword));
 
-    let requiredVocabularies = selectedProfile.vocabularies || [];
-    let missingVocabularies = requiredVocabularies.filter((requiredVocab) => {
-      if (!requiredVocab.id) return false;
-      return !(currentVocabularies.some((currentVocab) => currentVocab.thesaurus.identifier[0].identifier === requiredVocab.id));
-    });
-
-    if (missingVocabularies && missingVocabularies.length > 0) {
-      missingVocabularies.forEach((missing) => {
-        let missingVocab = allVocabularies.find((vocab) => vocab.citation.identifier[0].identifier === missing.id)
-        currentVocabularies.pushObject({
-          keyword: [],
-          keywordType: missingVocab.keywordType || 'theme',
-          thesaurus: copy(missingVocab.citation, true),
-          fullPath: true
-        })
-      });
-    }
-
-    set(info, 'keyword', A(currentVocabularies));
-    currentVocabularies.forEach((k, i) => {
+    //check to see if custom list
+    info.keyword.forEach((k) => {
       set(k, 'thesaurus', getWithDefault(k, 'thesaurus', {}));
-      set(k, 'thesaurus.identifier', getWithDefault(k, 'thesaurus.identifier', [{ identifier: 'custom' }]));
+      set(k, 'thesaurus.identifier', getWithDefault(k,
+        'thesaurus.identifier', [{
+          identifier: 'custom'
+        }]));
       set(k, 'thesaurus.date', getWithDefault(k, 'thesaurus.date', [{}]));
-      set(k, 'thesaurus.onlineResource', getWithDefault(k, 'thesaurus.onlineResource', [{}]));
+      set(k, 'thesaurus.onlineResource', getWithDefault(k,
+        'thesaurus.onlineResource', [{}]));
     });
 
-    return editModel;
+    return model;
   },
 
   actions: {
@@ -56,9 +38,8 @@ export default Route.extend(ScrollTo, {
       return this;
     },
     addThesaurus() {
-      console.log('addThesaurus');
-      let the = this.currentRouteModel().get( 'json.metadata.resourceInfo.keyword');
-      console.log('the', the);
+      let the = this.currentRouteModel().get(
+        'json.metadata.resourceInfo.keyword');
 
       $("html, body").animate({ scrollTop: $(document).height() }, "slow");
 
@@ -86,9 +67,9 @@ export default Route.extend(ScrollTo, {
       this.transitionTo('record.show.edit.keywords.thesaurus', id);
     },
     selectThesaurus(selected, thesaurus) {
-      console.log('selectThesaurus', selected, thesaurus);
       if(selected) {
-        set(thesaurus, 'thesaurus', copy(selected.citation, true));
+        set(thesaurus, 'thesaurus', copy(selected.citation,
+          true));
         if(selected.keywordType) {
           set(thesaurus, 'keywordType', selected.keywordType);
         }
