685e5141b6b61318922a437999d107ef6adcb27f
Devon Anderson <devon_anderson@fws.gov>
2023-08-07 13:09:52 -0400

    Fix clear cache storage modal to allow users to (#579)
    exit and cancel out of modal

--- app/router.js
+++ app/router.js
@@ -11,6 +11,7 @@ Router.map(function () {
   this.route('export');
   this.route('import');
   this.route('translate');
+  this.route('publish', function () { });
   this.route('help');
   this.route('settings', function() {
     this.route('validation');
