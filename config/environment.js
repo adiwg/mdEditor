cbc26a917ee3393c3e44101aa8a1d5056ddcce4e
Johnathan Aspinwall <jwaspin@gmail.com>
2023-10-13 08:49:31 -0600

    Update environment vars

--- config/environment.js
+++ config/environment.js
@@ -11,7 +11,7 @@ module.exports = function(environment) {
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
-    thesauriManifestUrl: 'https://cdn.jsdelivr.net/gh/adiwg/mdKeywords@temp/test/resources/manifest-develop.json',
+    thesauriManifestUrl: 'https://cdn.jsdelivr.net/gh/adiwg/mdKeywords@temp/test2/resources/manifest-develop.json',
     profilesManifestUrl: 'https://cdn.jsdelivr.net/gh/adiwg/mdProfiles@develop/resources/manifest.json',
     EmberENV: {
       FEATURES: {
