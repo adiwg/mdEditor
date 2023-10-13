5b059c13d4f10d0476dedf9092074caf562cc875
Johnathan Aspinwall <jwaspin@gmail.com>
2023-10-13 08:48:24 -0600

    Saving Progress

--- app/pods/components/input/md-select-thesaurus/component.js
+++ app/pods/components/input/md-select-thesaurus/component.js
@@ -31,60 +31,26 @@ export default Component.extend({
   selectThesaurus() {},
 
   thesaurusList: computed('keyword.thesaurus.[]', 'profile.profiles', 'recordProfile', function () {
-    let profileConfig = this.profile.profiles.find((p) => {
-      return p.id === this.recordProfile;
-    });
-    let vocabularies = profileConfig.vocabularies;
-
-    let defaultVocabularies = [
-      {
-        id: 'ISO 19115 Topic Category',
-        name: 'ISO 19115 Topic Category'
-      },
-      {
-        id: 'b2140059-b3ca-415c-b0a7-3e142783ffe8',
-        name: 'GCMD Instruments'
-      },
-      {
-        id: 'f3261de5-34c1-4980-af22-f9d7e7206d12',
-        name: 'GCMD Platforms'
-      },
-      {
-        id: '1eb0ea0a-312c-4d74-8d42-6f1ad758f999',
-        name: 'GCMD Science Keywords'
-      },
-      {
-        id: 'fa455d4a-5d87-56bc-b074-9a967beff904',
-        name: 'LCC Deliverables'
-      },
-      {
-        id: '425f4a7c-dca2-56d8-947e-6f6bd1033d70',
-        name: 'LCC End User Types'
-      },
-      {
-        id: '5da1d3b7-375b-58ae-a134-2ee0c94c395f',
-        name: 'LCC Project Category'
-      },
-    ]
-
-    let list = this.keyword
+    // const profileConfig = this.profile.profiles.find((p) => {
+    //   return p.id === this.recordProfile;
+    // });
+    // const profileThesauri = profileConfig.vocabularies;
+    const list = this.keyword
       .thesaurus
-      .filter((k) => {
-        if (vocabularies && vocabularies.length > 0) {
-          return vocabularies.some((v) => {
-            return v.id === k.citation.identifier[0].identifier;
-          });
-        } else {
-          return defaultVocabularies.some((v) => {
-            return v.id === k.citation.identifier[0].identifier;
-          });
-        }
-      })
+      // .filter((k) => {
+      //   if (profileThesauri && profileThesauri.length > 0) {
+      //     return profileThesauri.some((v) => {
+      //       return v.id === k.citation.identifier[0].identifier;
+      //     });
+      //   } else {
+      //     return this.keyword.manifest;
+      //   }
+      // })
       .map((k) => {
         return EmberObject.create({
           id: k.citation.identifier[0].identifier,
           label: k.label || k.citation.title || 'Keywords',
-          tooltipText: k.citation.description || 'No description available.',
+          // tooltipText: k.citation.description || 'No description available.',
         });
       })
       .sort((a, b) => {
@@ -94,7 +60,7 @@ export default Component.extend({
     list.unshift(EmberObject.create({
       id: 'custom',
       label: 'Custom Thesaurus',
-      tooltipText: "Select this option to use a custom thesaurus that you define yourself. This allows you to use your own set of keywords and categories that are specific to your project."
+      // tooltipText: "Select this option to use a custom thesaurus that you define yourself. This allows you to use your own set of keywords and categories that are specific to your project."
     }));
     return list;
   }),
