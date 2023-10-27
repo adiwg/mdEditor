30d3900696e44f193d1632bd105266d17201ef1f
Johnathan Aspinwall <jwaspin@gmail.com>
2023-10-27 10:27:08 -0600

    Refactor Load Thesauri

--- app/services/keyword.js
+++ app/services/keyword.js
@@ -1,18 +1,18 @@
-import Service from '@ember/service';
+import Service, { inject as service } from '@ember/service';
 import { A } from '@ember/array';
 import axios from 'axios';
 import ISO from 'mdcodes/resources/js/iso_topicCategory';
 import ENV from 'mdeditor/config/environment';
 
-export default class KeywordService extends Service {
-  thesaurus = A([]);
-  manifest = null;
+export default Service.extend({
+  thesaurus: A([]),
+  manifest: null,
+  profiles: service('custom-profile'),
 
-  constructor() {
-    super(...arguments);
+  init() {
+    this._super(...arguments);
     this.thesaurus.pushObject(this.generateIsoTopicCategory());
-    // Any other initialization code can go here.
-  }
+  },
 
   generateIsoTopicCategory() {
     const isoKeywords = ISO.codelist.map((topic) => ({
@@ -46,14 +46,15 @@ export default class KeywordService extends Service {
       keywordType: 'isoTopicCategory',
       label: 'ISO Topic Category',
       keywordsUrl: null,
+      isDefault: true,
     };
-  }
+  },
 
   findById(id) {
     return this.thesaurus.find((t) => {
       return t.citation.identifier[0].identifier === id;
     });
-  }
+  },
 
   async addThesaurus(thesaurus) {
     let keywords = null;
@@ -65,7 +66,7 @@ export default class KeywordService extends Service {
       thesaurus.keywords = keywords;
     }
     this.thesaurus.pushObject(thesaurus);
-  }
+  },
 
   async loadThesauri() {
     if (!ENV.thesauriManifestUrl) return;
@@ -74,12 +75,13 @@ export default class KeywordService extends Service {
       this.manifest = response.data;
       const promises = this.manifest.map((thesaurus) => {
         if (!thesaurus.url) return;
-        return axios.get(`${thesaurus.url}`);
+        return axios.get(thesaurus.url);
       });
       const responses = await Promise.all(promises);
       for (const [index, response] of responses.entries()) {
         if (!response) continue;
         const thesaurus = response.data;
+        thesaurus.isDefault = true;
         await this.addThesaurus(thesaurus);
         this.manifest[index].identifier = thesaurus.citation.identifier[0].identifier;
         this.manifest[index].isDefault = true;
@@ -90,8 +92,29 @@ export default class KeywordService extends Service {
         isDefault: true,
         url: null,
       });
+      // load unique thesauri from custom profiles
+      const profiles = this.profiles.profiles;
+      const uniqueThesauri = [];
+      profiles.forEach((profile) => {
+        profile.thesauri?.forEach((thesaurus) => {
+          const existingThesaurus = this.manifest.find((t) => t.url === thesaurus.url);
+          if (existingThesaurus) return;
+          const existingUniqueThesaurus = uniqueThesauri.find((t) => t.url === thesaurus.url);
+          if (existingUniqueThesaurus) return;
+          uniqueThesauri.push(thesaurus);
+        }
+      )});
+      uniqueThesauri.forEach(async (thesaurus) => {
+        const existingThesaurus = this.manifest.find((t) => t.url === thesaurus.url);
+        if (existingThesaurus) return;
+        const response = await axios.get(thesaurus.url);
+        const thesaurusData = response.data;
+        thesaurus.identifier = thesaurusData.citation.identifier[0].identifier;
+        this.manifest.push(thesaurus);
+        await this.addThesaurus(thesaurusData);
+      });
     } catch (error) {
       console.error(error);
     }
   }
-}
+});
