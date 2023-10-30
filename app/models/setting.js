2969d718139903ece6e3b73cb8fd5d991b0291ae
Johnathan Aspinwall <jwaspin@gmail.com>
2023-10-30 13:38:05 -0600

    Use Prod Urls

--- app/models/setting.js
+++ app/models/setting.js
@@ -5,10 +5,10 @@ import { inject as service } from '@ember/service';
 import EmberObject, { observer } from "@ember/object";
 
 const defaultValues = {
-  // mdTranslatorAPI: 'https://api.sciencebase.gov/mdTranslator/api/v3/translator',
-  // itisProxyUrl: 'https://api.sciencebase.gov/mdTranslator',
-  mdTranslatorAPI: 'https://dev-mdtranslator.mdeditor.org/api/v3/translator',
-  itisProxyUrl: 'https://dev-mdtranslator.mdeditor.org',
+  mdTranslatorAPI: 'https://api.sciencebase.gov/mdTranslator/api/v3/translator',
+  itisProxyUrl: 'https://api.sciencebase.gov/mdTranslator',
+  // mdTranslatorAPI: 'https://dev-mdtranslator.mdeditor.org/api/v3/translator',
+  // itisProxyUrl: 'https://dev-mdtranslator.mdeditor.org',
   fiscalStartMonth: '10'
 };
 
