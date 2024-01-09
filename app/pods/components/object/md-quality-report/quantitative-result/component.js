fad9758cb3e96b84a71d482e1ea49887809352e9
Johnathan Aspinwall <jwaspin@gmail.com>
2024-01-09 14:06:10 -0700

    Complete Quantitative Result Object

--- app/pods/components/object/md-quality-report/quantitative-result/component.js
+++ app/pods/components/object/md-quality-report/quantitative-result/component.js
@@ -1,4 +1,5 @@
 import Component from "@ember/component";
+import { computed } from "@ember/object";
 import { once } from "@ember/runloop";
 import { alias } from "@ember/object/computed";
 import { set, getWithDefault } from "@ember/object";
@@ -11,8 +12,34 @@ export default Component.extend({
 
     if (model) {
       once(this, function () {
-        set(model, "scope", getWithDefault(model, "scope", {}));
+        set(
+          model,
+          "scope",
+          getWithDefault(model, "scope", { scopeDescription: [] })
+        );
+        set(model, "value", getWithDefault(model, "value", []));
       });
     }
   },
+
+  name: "",
+
+  quantitativeValue: computed("model.value.[]", {
+    get() {
+      const valueArray = this.get("model.value");
+      return valueArray && valueArray.length > 0 ? valueArray[0] : null;
+    },
+    set(key, value) {
+      const valueArray = this.get("model.value");
+      if (valueArray && valueArray.length > 0) {
+        valueArray[0] = parseInt(value);
+      } else {
+        this.set("model.value", [parseInt(value)]);
+      }
+      return value;
+    },
+  }),
+
+  units: alias("model.valueUnits"),
+  scopeCode: alias("model.scope.scopeCode"),
 });
