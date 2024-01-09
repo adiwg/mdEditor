15d243df2a3756efb8a9dadfd189a7eded867bd4
Johnathan Aspinwall <jwaspin@gmail.com>
2024-01-09 07:09:24 -0700

    New and Improved
    Quality Measure Name handled as an array.
    Renamed 'type' to 'reportType'.
    Added Evaluation Type.
    Disabling coverage result.
    Standalone Quality Report Details changed to textarea.

--- app/pods/components/object/md-quality-report/component.js
+++ app/pods/components/object/md-quality-report/component.js
@@ -1,4 +1,5 @@
 import Component from "@ember/component";
+import { computed } from "@ember/object";
 import { once } from "@ember/runloop";
 import { alias } from "@ember/object/computed";
 import { set, getWithDefault } from "@ember/object";
@@ -14,7 +15,7 @@ export default Component.extend({
         set(
           model,
           "qualityMeasure",
-          getWithDefault(model, "qualityMeasure", {})
+          getWithDefault(model, "qualityMeasure", { name: [] })
         );
         set(
           model,
@@ -45,5 +46,22 @@ export default Component.extend({
     }
   },
 
-  type: alias("model.type"),
+  qualityMeasureName: computed("model.qualityMeasure.name.[]", {
+    get() {
+      let nameArray = this.get("model.qualityMeasure.name");
+      return nameArray && nameArray.length > 0 ? nameArray[0] : null;
+    },
+    set(key, value) {
+      let nameArray = this.get("model.qualityMeasure.name");
+      if (nameArray && nameArray.length > 0) {
+        nameArray[0] = value;
+      } else {
+        this.set("model.qualityMeasure.name", [value]);
+      }
+      return value;
+    },
+  }),
+
+  reportType: alias("model.type"),
+  evaluationType: alias("model.evaluationMethod.type"),
 });
