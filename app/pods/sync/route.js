747d99ceeb082388bff63f074fc7a5997e4551eb
Frank Aiello <frankvaiello@gmail.com>
2024-01-30 10:42:37 -0500

    Adds session persistence for logged in users

--- app/pods/sync/route.js
+++ app/pods/sync/route.js
@@ -1,5 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class SyncRoute extends Route {
+  @service couch;
 
+  async beforeModel() {
+    await this.couch.setup();
+  }
 }
--- app/services/couch.js
+++ app/services/couch.js
@@ -10,23 +10,29 @@ export default class CouchService extends Service {
   @tracked loggedIn = false;
   @tracked username = null;
   // DB instance data
-  local = null;
-  remote = null;
+  localDb = null;
+  remoteDb = null;
   // DB data Loading state
   @tracked replicationState = null;
   @tracked replicationMessage = null;
+  // Couch model data
+  couch = null;
 
-  @action
-  async login(remoteUrl, remoteName, username, password) {
-    const remote = new PouchDB(`${remoteUrl}/${remoteName}`, { skip_setup: true });
+  async setup() {
+    const adapter = this.store.adapterFor('application');
+    this.localDb = adapter.db;
+    // Check to see if they have a remote name and url saved
+    // If they do, then getSession to see if their cookie is still valid
+    // If valid, then display remote url, remote name, and user name
     try {
-      const user = await remote.login(username, password);
-      if (user.ok) {
-        this.username = user.name;
-        this.remote = remote;
-        const adapter = this.store.adapterFor('application');
-        this.local = adapter.db;
-        this.loggedIn = true;
+      const couch = await this.getCouch();
+      if (couch) {
+        this.couch = couch;
+        this.setRemoteDb(this.couch.remoteUrl, this.couch.remoteName);
+        const session = await this.remoteDb.getSession();
+        if (session.userCtx.name) {
+          this.setLoggedInUser(session.userCtx.name);
+        }
       }
     } catch(e) {
       console.log(e);
@@ -34,9 +40,57 @@ export default class CouchService extends Service {
   }
 
   @action
+  async login(remoteUrl, remoteName, username, password) {
+    try {
+      const user = await this.remoteDb.login(username, password);
+      if (user.ok) {
+        this.setLoggedInUser(user.name);
+        this.setCouch(remoteUrl, remoteName);
+        this.setRemoteDb(remoteUrl, remoteName);
+      }
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  async getCouch() {
+    const store = await this.store.findAll('couch');
+    const couch = store.get('firstObject');
+    return couch;
+  }
+
+  async setCouch(remoteUrl, remoteName) {
+    const couch = await this.getCouch();
+    try {
+      // Update
+      if (couch) {
+        couch.remoteUrl = remoteUrl;
+        couch.remoteName = remoteName;
+        await couch.save()
+      }
+      // Create
+      else {
+        const couch = this.store.createRecord('couch', { remoteUrl, remoteName });
+        await couch.save();
+      }
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  setRemoteDb(remoteUrl, remoteName) {
+    this.remoteDb = new PouchDB(`${remoteUrl}/${remoteName}`);
+  }
+
+  setLoggedInUser(name) {
+    this.username = name;
+    this.loggedIn = true;
+  }
+
+  @action
   push() {
     this.replicationState = 'Pushing';
-    this.local.replicate.to(this.remote)
+    this.localDb.replicate.to(this.remoteDb)
       .on('complete', () => {
         this.displayMessage(this.replicationState);
         this.replicationState = null;
@@ -49,7 +103,7 @@ export default class CouchService extends Service {
   @action
   pull() {
     this.replicationState = 'Pulling';
-    this.local.replicate.from(this.remote)
+    this.localDb.replicate.from(this.remoteDb)
       .on('complete', () => {
         this.displayMessage(this.replicationState);
         this.replicationState = null;
@@ -62,7 +116,7 @@ export default class CouchService extends Service {
   @action
   sync() {
     this.replicationState = 'Syncing';
-    this.couch.sync(this.remote)
+    this.localDb.sync(this.remoteDb)
       .on('complete', () => {
         this.displayMessage(this.replicationState);
         this.replicationState = null;
