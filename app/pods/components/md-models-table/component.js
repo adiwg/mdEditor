543cfc7c396b35112e9fd813c355701064ef2e9a
Devon Anderson <devon_anderson@fws.gov>
2024-02-05 22:32:17 -0500

    Clean up logic for extent to allow creation and viewing of features (#640)

--- app/pods/components/md-models-table/component.js
+++ app/pods/components/md-models-table/component.js
@@ -1,11 +1,7 @@
 import Table from 'ember-models-table/components/models-table';
 import Theme from './themes/bootstrap3';
 
-export default Table.extend({
-  init() {
-    this._super(...arguments);
-    // this.pageSizeValues = [10, 25, 50, 500];
-    // this.filteringIgnoreCase = true;
-  },
-  themeInstance: Theme.create()
-});
+
+export default class MdModelsTableComponent extends Table {
+  themeInstance = Theme.create();
+}
