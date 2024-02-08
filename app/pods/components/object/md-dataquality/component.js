0be2116a924fb15f446f9f26ad337de7c239205d
Johnathan Aspinwall <jwaspin@gmail.com>
2024-02-07 17:28:20 -0700

    Revert back to single quotes

--- app/pods/components/object/md-dataquality/component.js
+++ app/pods/components/object/md-dataquality/component.js
@@ -1,34 +1,34 @@
-import Component from "@ember/component";
-import { getWithDefault, get, set } from "@ember/object";
-import { once } from "@ember/runloop";
-import uuidV4 from "uuid/v4";
+import Component from '@ember/component';
+import { getWithDefault, get, set } from '@ember/object';
+import { once } from '@ember/runloop';
+import uuidV4 from 'uuid/v4';
 
 export default Component.extend({
   didReceiveAttrs() {
     this._super(...arguments);
 
-    let model = get(this, "model");
+    let model = get(this, 'model');
 
     once(function () {
-      set(model, "scope", getWithDefault(model, "scope", {}));
+      set(model, 'scope', getWithDefault(model, 'scope', {}));
       set(
         model,
-        "systemIdentifier",
-        getWithDefault(model, "systemIdentifier", { uid: uuidV4() })
+        'systemIdentifier',
+        getWithDefault(model, 'systemIdentifier', { uid: uuidV4() })
       );
-      set(model, "report", getWithDefault(model, "report", []));
+      set(model, 'report', getWithDefault(model, 'report', []));
     });
   },
 
-  tagName: "form",
+  tagName: 'form',
 
   actions: {
     addStandaloneQualityReport() {
-      set(this.model, "standaloneQualityReport", { abstract: "" });
+      set(this.model, 'standaloneQualityReport', { abstract: '' });
     },
 
     deleteStandaloneQualityReport() {
-      set(this.model, "standaloneQualityReport", undefined);
+      set(this.model, 'standaloneQualityReport', undefined);
     },
   },
 });
