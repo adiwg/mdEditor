c6bbc271cc7302bb2d74d84320ecf5b76438de8c
Johnathan Aspinwall <jwaspin@gmail.com>
2024-03-11 14:02:38 -0600

    Update conformance result component to display pass/fail status

--- app/pods/components/object/md-quality-report/conformance-result/component.js
+++ app/pods/components/object/md-quality-report/conformance-result/component.js
@@ -1,7 +1,7 @@
 import Component from '@ember/component';
 import { once } from '@ember/runloop';
 import { alias } from '@ember/object/computed';
-import { set, getWithDefault } from '@ember/object';
+import { computed, set, getWithDefault } from '@ember/object';
 
 export default Component.extend({
   didReceiveAttrs() {
@@ -20,4 +20,9 @@ export default Component.extend({
 
   name: '',
   scopeCode: alias('model.scope.scopeCode'),
+  resultText: computed('model.pass', function () {
+    return this.get('model.pass')
+      ? 'The result passes conformance.'
+      : 'The result does not pass conformance.';
+  }),
 });
