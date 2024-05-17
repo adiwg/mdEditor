d4491d85d5a9d2df5b82834051c3e4b8bbbcfcfb
Johnathan Aspinwall <jwaspin@gmail.com>
2024-05-17 10:29:34 -0600

    feat: Update md-datetime component to use single quotes consistently

--- app/pods/components/input/md-datetime/component.js
+++ app/pods/components/input/md-datetime/component.js
@@ -6,13 +6,13 @@
  * @submodule components-input
  */
 
-import Component from "@ember/component";
-import { isBlank } from "@ember/utils";
-import { get, set, computed, defineProperty } from "@ember/object";
-import { once } from "@ember/runloop";
-import { assert, debug } from "@ember/debug";
-import moment from "moment";
-import dayjs from "dayjs";
+import Component from '@ember/component';
+import { isBlank } from '@ember/utils';
+import { get, set, computed, defineProperty } from '@ember/object';
+import { once } from '@ember/runloop';
+import { assert, debug } from '@ember/debug';
+import moment from 'moment';
+import dayjs from 'dayjs';
 
 export default Component.extend({
   /**
@@ -42,11 +42,11 @@ export default Component.extend({
 
       defineProperty(
         this,
-        "_date",
+        '_date',
         computed(`model.${valuePath}`, {
           get() {
             let val = get(this, `model.${valuePath}`);
-            return val ? moment(val, this.get("altFormat" || null)) : null;
+            return val ? moment(val, this.get('altFormat' || null)) : null;
           },
           set(key, value) {
             let formatted = this.formatValue(value, `model.${valuePath}`);
@@ -57,41 +57,41 @@ export default Component.extend({
     } else {
       defineProperty(
         this,
-        "_date",
-        computed("date", {
+        '_date',
+        computed('date', {
           get() {
             let val = this.date;
-            return val ? moment(val, this.get("altFormat" || null)) : null;
+            return val ? moment(val, this.get('altFormat' || null)) : null;
           },
           set(key, value) {
-            let formatted = this.formatValue(value, "date");
+            let formatted = this.formatValue(value, 'date');
             return formatted;
           },
         })
       );
     }
   },
-  classNames: ["md-datetime", "md-input-input"],
-  classNameBindings: ["label:form-group", "required"],
+  classNames: ['md-datetime', 'md-input-input'],
+  classNameBindings: ['label:form-group', 'required'],
 
   date: null,
-  format: "YYYY-MM-DD HH:mm:ss",
-  placeholder: "Enter date and time",
+  format: 'YYYY-MM-DD HH:mm:ss',
+  placeholder: 'Enter date and time',
   label: null,
-  useCurrent: "day",
+  useCurrent: 'day',
   showTodayButton: true,
   showClear: true,
 
   formatValue(value, target) {
     if (isBlank(value)) {
-      once(this, "setTargetToNull", target);
+      once(this, 'setTargetToNull', target);
       return value;
     }
 
     let formattedDate = dayjs(value).format(this.format); // Use the format directly
 
     if (formattedDate !== this.get(target)) {
-      once(this, "updateFormattedDate", formattedDate, target);
+      once(this, 'updateFormattedDate', formattedDate, target);
     }
 
     return formattedDate;
@@ -107,14 +107,14 @@ export default Component.extend({
 
   calendarIcons: computed(function () {
     return {
-      time: "fa fa-clock-o",
-      date: "fa fa-calendar",
-      up: "fa fa-chevron-up",
-      down: "fa fa-chevron-down",
-      previous: "fa fa-angle-double-left",
-      next: "fa fa-angle-double-right",
-      close: "fa fa-times",
-      clear: "fa fa-trash",
+      time: 'fa fa-clock-o',
+      date: 'fa fa-calendar',
+      up: 'fa fa-chevron-up',
+      down: 'fa fa-chevron-down',
+      previous: 'fa fa-angle-double-left',
+      next: 'fa fa-angle-double-right',
+      close: 'fa fa-times',
+      clear: 'fa fa-trash',
     };
   }),
 });
