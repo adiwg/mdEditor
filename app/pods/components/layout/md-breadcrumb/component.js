7530e0050b145714204e01f445c7eaf7a1715673
dvonanderson <dvonanderson@gmail.com>
2026-01-21 18:09:51 -0500

    - Upgrade `ember-cli-htmlbars`, `ember-cp-validations`, `ember-elsewhere`, `ember-modal-dialog`, `ember-moment` `liquid-fire` to versions that help surpress deprecations. - Add resolutions for for `ember-cli-htmlbars` - Refactored breadcrumbs component to be self sufficient instead of using `ember-crumbly` and removed `ember-crumbly`

--- app/pods/components/layout/md-breadcrumb/component.js
+++ app/pods/components/layout/md-breadcrumb/component.js
@@ -1,5 +1,16 @@
-import classic from 'ember-classic-decorator';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
 
-@classic
-export default class MdBreadcrumbComponent extends Component {}
+/**
+ * Breadcrumb navigation component that displays the current route hierarchy.
+ * Uses the breadcrumbs service to get route information.
+ *
+ * @class MdBreadcrumbComponent
+ */
+export default class MdBreadcrumbComponent extends Component {
+  @service breadcrumbs;
+
+  get routeHierarchy() {
+    return this.breadcrumbs.routeHierarchy;
+  }
+}
